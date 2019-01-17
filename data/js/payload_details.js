f1f7553f1aaf2c1b2302c5a0334fc24595c7dae3
drjwlowery <jwlowery@gmail.com>
2019-01-16 20:17:50 -0500

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 280,
-	"created_at": "Wed Jan 16 01:17:50 +0000 2019",
+	"tweets": 282,
+	"created_at": "Thu Jan 17 01:17:49 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,608 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Wed Jan 16 16:09:39 +0000 2019",
+		"id": 1085569783674875900,
+		"id_str": "1085569783674875905",
+		"full_text": "Thomas Arkle Clark was @NASPAtweets 2nd president &amp; hosted the 2nd conference at @Illinois_Alma. You can read his book, Discipline and the Derelict: Being a Series of Essays on Some of Those who Tread the Green Carpet (1922) on Google Books.  https://t.co/1HGamImyvB #NASPA100 https://t.co/k8hhaNkLEU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NASPA100",
+					"indices": [
+						271,
+						280
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						23,
+						35
+					]
+				},
+				{
+					"screen_name": "Illinois_Alma",
+					"name": "University of Illinois",
+					"id": 18165866,
+					"id_str": "18165866",
+					"indices": [
+						85,
+						99
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/1HGamImyvB",
+					"expanded_url": "http://ow.ly/5N9Pv",
+					"display_url": "ow.ly/5N9Pv",
+					"indices": [
+						247,
+						270
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1085569782169120800,
+					"id_str": "1085569782169120769",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/DxC39x0WsAE2zLl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxC39x0WsAE2zLl.jpg",
+					"url": "https://t.co/k8hhaNkLEU",
+					"display_url": "pic.twitter.com/k8hhaNkLEU",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1085569783674875905/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 418,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 418,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 355,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1085569782169120800,
+					"id_str": "1085569782169120769",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/DxC39x0WsAE2zLl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxC39x0WsAE2zLl.jpg",
+					"url": "https://t.co/k8hhaNkLEU",
+					"display_url": "pic.twitter.com/k8hhaNkLEU",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1085569783674875905/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 418,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 418,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 355,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/ytol9MXOYA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ytol9MXOYA",
+							"expanded_url": "http://naspa.edu",
+							"display_url": "naspa.edu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 148,
+			"friends_count": 17,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 39,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 263,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thomas Arkle Clark was @NASPAtweets 2nd president &amp; hosted the 2nd conference at @Illinois_Alma. You can read his book, Discipline and the Derelict: Being a Series of Essays on Some of Those who Tread the Green Carpet (1922) on Google Books.  https://t.co/1HGamImyvB #NASPA100 https://t.co/k8hhaNkLEU"
+	},
+	{
+		"created_at": "Tue Jan 15 17:04:41 +0000 2019",
+		"id": 1085221242779648000,
+		"id_str": "1085221242779648003",
+		"full_text": "Dr. Larry Ebbers recruited me to the @IowaStateU Higher Ed doctoral pgm &amp; served on my committee! #Thankful @chs_iastate @iowastate https://t.co/ZJPSgdpiqp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			135
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Thankful",
+					"indices": [
+						102,
+						111
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "IowaStateU",
+					"name": "Iowa State University",
+					"id": 211258879,
+					"id_str": "211258879",
+					"indices": [
+						37,
+						48
+					]
+				},
+				{
+					"screen_name": "chs_iastate",
+					"name": "College of Human Sci",
+					"id": 74795779,
+					"id_str": "74795779",
+					"indices": [
+						112,
+						124
+					]
+				},
+				{
+					"screen_name": "IowaState",
+					"name": "Iowa State ",
+					"id": 2611971,
+					"id_str": "2611971",
+					"indices": [
+						125,
+						135
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ZJPSgdpiqp",
+					"expanded_url": "https://twitter.com/naspahistory/status/1085213838885208064",
+					"display_url": "twitter.com/naspahistory/s…",
+					"indices": [
+						136,
+						159
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 746377300225065000,
+			"id_str": "746377300225064960",
+			"name": "Juan R Guardia, PhD",
+			"screen_name": "JRGuardiaUCDOS",
+			"location": "Cincinnati, OH",
+			"description": "AVPSA & Dean of Students @UofCincy 🐾 • @Phiota ΦΙΑ 🦁• @MDCollege, @FloridaState & @IowaStateU Alumnus • 🇨🇺 from Miami (FL)",
+			"url": "https://t.co/GMHpj1L584",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GMHpj1L584",
+							"expanded_url": "http://www.uc.edu/sa.html",
+							"display_url": "uc.edu/sa.html",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1220,
+			"friends_count": 1054,
+			"listed_count": 15,
+			"created_at": "Fri Jun 24 16:19:50 +0000 2016",
+			"favourites_count": 6142,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6474,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/852841071797252096/fcsDNr7e_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/852841071797252096/fcsDNr7e_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/746377300225064960/1466785392",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "e444ecd51bd16ff3",
+			"url": "https://api.twitter.com/1.1/geo/id/e444ecd51bd16ff3.json",
+			"place_type": "city",
+			"name": "Cincinnati",
+			"full_name": "Cincinnati, OH",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-84.710722,
+							39.052962
+						],
+						[
+							-84.3689026,
+							39.052962
+						],
+						[
+							-84.3689026,
+							39.2211137
+						],
+						[
+							-84.710722,
+							39.2211137
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1085213838885208000,
+		"quoted_status_id_str": "1085213838885208064",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ZJPSgdpiqp",
+			"expanded": "https://twitter.com/naspahistory/status/1085213838885208064",
+			"display": "twitter.com/naspahistory/s…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jan 15 16:35:16 +0000 2019",
+			"id": 1085213838885208000,
+			"id_str": "1085213838885208064",
+			"full_text": "Writing in 2010 for @NASPAtweets' Legacy Scrapbook, former @NASPApres Larry Ebbers urged student affairs professionals to learn from their mentors as well as understanding we all leave a legacy. During his presidency, he was a professor and department chair at @IowaStateU https://t.co/QMtLlrXBFb",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				272
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "NASPAtweets",
+						"name": "NASPA",
+						"id": 93729105,
+						"id_str": "93729105",
+						"indices": [
+							20,
+							32
+						]
+					},
+					{
+						"screen_name": "NASPApres",
+						"name": "Kevin Kruger",
+						"id": 95735176,
+						"id_str": "95735176",
+						"indices": [
+							59,
+							69
+						]
+					},
+					{
+						"screen_name": "IowaStateU",
+						"name": "Iowa State University",
+						"id": 211258879,
+						"id_str": "211258879",
+						"indices": [
+							261,
+							272
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1085213837010309100,
+						"id_str": "1085213837010309120",
+						"indices": [
+							273,
+							296
+						],
+						"media_url": "http://pbs.twimg.com/media/Dw90PCxWkAAC6Jr.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dw90PCxWkAAC6Jr.jpg",
+						"url": "https://t.co/QMtLlrXBFb",
+						"display_url": "pic.twitter.com/QMtLlrXBFb",
+						"expanded_url": "https://twitter.com/NASPAhistory/status/1085213838885208064/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1084,
+								"h": 1006,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 631,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1084,
+								"h": 1006,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1085213837010309100,
+						"id_str": "1085213837010309120",
+						"indices": [
+							273,
+							296
+						],
+						"media_url": "http://pbs.twimg.com/media/Dw90PCxWkAAC6Jr.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dw90PCxWkAAC6Jr.jpg",
+						"url": "https://t.co/QMtLlrXBFb",
+						"display_url": "pic.twitter.com/QMtLlrXBFb",
+						"expanded_url": "https://twitter.com/NASPAhistory/status/1085213838885208064/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1084,
+								"h": 1006,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 631,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1084,
+								"h": 1006,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 937716186057527300,
+				"id_str": "937716186057527297",
+				"name": "NASPA History",
+				"screen_name": "NASPAhistory",
+				"location": "",
+				"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+				"url": "https://t.co/ytol9MXOYA",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ytol9MXOYA",
+								"expanded_url": "http://naspa.edu",
+								"display_url": "naspa.edu",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 148,
+				"friends_count": 17,
+				"listed_count": 0,
+				"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+				"favourites_count": 39,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 263,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 5,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 6,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dr. Larry Ebbers recruited me to the @IowaStateU Higher Ed doctoral pgm &amp; served on my committee! #Thankful @chs_iastate @iowastate https://t.co/ZJPSgdpiqp"
+	},
+	{
 		"created_at": "Tue Jan 15 17:04:41 +0000 2019",
 		"id": 1085221242779648000,
 		"id_str": "1085221242779648003",
