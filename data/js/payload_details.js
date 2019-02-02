9315b29f1b167197bf3fdcb08a4ada29fd9ca43d
drjwlowery <jwlowery@gmail.com>
2019-02-01 20:17:50 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 294,
-	"created_at": "Tue Jan 29 01:17:50 +0000 2019",
+	"tweets": 295,
+	"created_at": "Sat Feb 02 01:17:49 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_02.js
@@ -0,0 +1,198 @@
+Grailbird.data.tweets_2019_02 = 
+[
+	{
+		"created_at": "Fri Feb 01 17:35:53 +0000 2019",
+		"id": 1091389687263121400,
+		"id_str": "1091389687263121408",
+		"full_text": "On Feb. 1, 1960, 4 African-American students from @ncatsuaggies sat in at the Woolworth's lunch counter in Greensboro and launched a movement. In 1961, @NASPAtweets invited Howard President &amp; lawyer James Nabrit, Jr. to speak  about the sit-ins and student rights more broadly. https://t.co/ZDH2jKbHcQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			281
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ncatsuaggies",
+					"name": "North Carolina A&T",
+					"id": 52487302,
+					"id_str": "52487302",
+					"indices": [
+						50,
+						63
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						152,
+						164
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1091389685514031100,
+					"id_str": "1091389685514031104",
+					"indices": [
+						282,
+						305
+					],
+					"media_url": "http://pbs.twimg.com/media/DyVlIvSW0AAvnll.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyVlIvSW0AAvnll.jpg",
+					"url": "https://t.co/ZDH2jKbHcQ",
+					"display_url": "pic.twitter.com/ZDH2jKbHcQ",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1091389687263121408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 411,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1020,
+							"h": 617,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1020,
+							"h": 617,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1091389685514031100,
+					"id_str": "1091389685514031104",
+					"indices": [
+						282,
+						305
+					],
+					"media_url": "http://pbs.twimg.com/media/DyVlIvSW0AAvnll.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyVlIvSW0AAvnll.jpg",
+					"url": "https://t.co/ZDH2jKbHcQ",
+					"display_url": "pic.twitter.com/ZDH2jKbHcQ",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1091389687263121408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 411,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1020,
+							"h": 617,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1020,
+							"h": 617,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/ytol9MXOYA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ytol9MXOYA",
+							"expanded_url": "http://naspa.edu",
+							"display_url": "naspa.edu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 149,
+			"friends_count": 17,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 41,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 276,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On Feb. 1, 1960, 4 African-American students from @ncatsuaggies sat in at the Woolworth's lunch counter in Greensboro and launched a movement. In 1961, @NASPAtweets invited Howard President &amp; lawyer James Nabrit, Jr. to speak  about the sit-ins and student rights more broadly. https://t.co/ZDH2jKbHcQ"
+	}
+]
