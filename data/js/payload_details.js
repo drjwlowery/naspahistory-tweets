3c4aaa10c9680316a9be9d24eb0d577b0b9b4872
drjwlowery <jwlowery@gmail.com>
2019-02-02 20:17:50 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 295,
-	"created_at": "Sat Feb 02 01:17:49 +0000 2019",
+	"tweets": 296,
+	"created_at": "Sun Feb 03 01:17:49 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sat Feb 02 14:34:52 +0000 2019",
+		"id": 1091706520650375200,
+		"id_str": "1091706520650375168",
+		"full_text": "@jorgeaflorimon @Brian_Garsh @NASPAtweets NASPA members can read online and download a PDF copy of the entire issue.",
+		"truncated": false,
+		"display_text_range": [
+			42,
+			116
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jorgeaflorimon",
+					"name": "Jorge Almonte-Florimon, Ed.M",
+					"id": 830045089196101600,
+					"id_str": "830045089196101632",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "Brian_Garsh",
+					"name": "Brian Garsh, M.Ed.",
+					"id": 710250134379044900,
+					"id_str": "710250134379044864",
+					"indices": [
+						16,
+						28
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						29,
+						41
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1091698432501338100,
+		"in_reply_to_status_id_str": "1091698432501338115",
+		"in_reply_to_user_id": 830045089196101600,
+		"in_reply_to_user_id_str": "830045089196101632",
+		"in_reply_to_screen_name": "jorgeaflorimon",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/ytol9MXOYA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ytol9MXOYA",
+							"expanded_url": "http://naspa.edu",
+							"display_url": "naspa.edu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 150,
+			"friends_count": 17,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 41,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 277,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@jorgeaflorimon @Brian_Garsh @NASPAtweets NASPA members can read online and download a PDF copy of the entire issue."
+	},
+	{
 		"created_at": "Fri Feb 01 17:35:53 +0000 2019",
 		"id": 1091389687263121400,
 		"id_str": "1091389687263121408",
