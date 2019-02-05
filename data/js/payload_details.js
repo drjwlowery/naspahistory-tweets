3a9b45710e7dff23ea388b06113960241923e99e
drjwlowery <jwlowery@gmail.com>
2019-02-04 20:17:50 -0500

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 296,
-	"created_at": "Sun Feb 03 01:17:49 +0000 2019",
+	"tweets": 298,
+	"created_at": "Tue Feb 05 01:17:49 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,393 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Mon Feb 04 22:35:05 +0000 2019",
+		"id": 1092552147160522800,
+		"id_str": "1092552147160522752",
+		"full_text": "January seems to have sped by, and February is in full swing. Check out this week's SA Weekly for the latest higher ed news, NASPA research and policy, constituent blogs, and more.\nhttps://t.co/SnUS0mjhug https://t.co/cuUCQJxrXR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			204
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SnUS0mjhug",
+					"expanded_url": "https://www.naspa.org/about/blog/sa-weekly-february-4",
+					"display_url": "naspa.org/about/blog/sa-…",
+					"indices": [
+						181,
+						204
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1092552145197559800,
+					"id_str": "1092552145197559808",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/DymGYzEXcAAiIgS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DymGYzEXcAAiIgS.jpg",
+					"url": "https://t.co/cuUCQJxrXR",
+					"display_url": "pic.twitter.com/cuUCQJxrXR",
+					"expanded_url": "https://twitter.com/NASPAtweets/status/1092552147160522752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 599,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 407,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 599,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1092552145197559800,
+					"id_str": "1092552145197559808",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/media/DymGYzEXcAAiIgS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DymGYzEXcAAiIgS.jpg",
+					"url": "https://t.co/cuUCQJxrXR",
+					"display_url": "pic.twitter.com/cuUCQJxrXR",
+					"expanded_url": "https://twitter.com/NASPAtweets/status/1092552147160522752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 599,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 407,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 599,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 93729105,
+			"id_str": "93729105",
+			"name": "NASPA",
+			"screen_name": "NASPAtweets",
+			"location": "Washington, DC",
+			"description": "We empower student affairs practitioners working in higher ed to serve the changing academic, emotional, and social needs of today's diverse student population.",
+			"url": "https://t.co/zca501mzTd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zca501mzTd",
+							"expanded_url": "http://www.naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 31344,
+			"friends_count": 308,
+			"listed_count": 864,
+			"created_at": "Mon Nov 30 22:49:50 +0000 2009",
+			"favourites_count": 6266,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 13933,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1019536617831198720/zjruThqD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1019536617831198720/zjruThqD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/93729105/1544716574",
+			"profile_link_color": "006666",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EEEEEE",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "January seems to have sped by, and February is in full swing. Check out this week's SA Weekly for the latest higher ed news, NASPA research and policy, constituent blogs, and more.\nhttps://t.co/SnUS0mjhug https://t.co/cuUCQJxrXR"
+	},
+	{
+		"created_at": "Mon Feb 04 17:45:18 +0000 2019",
+		"id": 1092479220461289500,
+		"id_str": "1092479220461289473",
+		"full_text": "The first woman we have clear records of attending the @NASPAtweets as a member is Dr. Margaret L. Johnson. She was the dean of students at Richmond Polytechnic Institute (now @VCU). #NASPA100 https://t.co/gKiw8xIT7J",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			192
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NASPA100",
+					"indices": [
+						183,
+						192
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						55,
+						67
+					]
+				},
+				{
+					"screen_name": "VCU",
+					"name": "VCU",
+					"id": 156714051,
+					"id_str": "156714051",
+					"indices": [
+						176,
+						180
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1092479218674483200,
+					"id_str": "1092479218674483200",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/DylED6mWwAA1PQv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DylED6mWwAA1PQv.jpg",
+					"url": "https://t.co/gKiw8xIT7J",
+					"display_url": "pic.twitter.com/gKiw8xIT7J",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1092479220461289473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 693,
+							"h": 450,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 693,
+							"h": 450,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 442,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1092479218674483200,
+					"id_str": "1092479218674483200",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/DylED6mWwAA1PQv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DylED6mWwAA1PQv.jpg",
+					"url": "https://t.co/gKiw8xIT7J",
+					"display_url": "pic.twitter.com/gKiw8xIT7J",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1092479220461289473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 693,
+							"h": 450,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 693,
+							"h": 450,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 442,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/ytol9MXOYA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ytol9MXOYA",
+							"expanded_url": "http://naspa.edu",
+							"display_url": "naspa.edu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 149,
+			"friends_count": 17,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 41,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 279,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The first woman we have clear records of attending the @NASPAtweets as a member is Dr. Margaret L. Johnson. She was the dean of students at Richmond Polytechnic Institute (now @VCU). #NASPA100 https://t.co/gKiw8xIT7J"
+	},
+	{
 		"created_at": "Sat Feb 02 14:34:52 +0000 2019",
 		"id": 1091706520650375200,
 		"id_str": "1091706520650375168",
