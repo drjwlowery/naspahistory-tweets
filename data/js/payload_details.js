24f1678aafa45366b3c784609933bbce93aceb84
drjwlowery <jwlowery@gmail.com>
2019-03-04 20:17:50 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 320,
-	"created_at": "Thu Feb 28 01:17:49 +0000 2019",
+	"tweets": 321,
+	"created_at": "Tue Mar 05 01:17:49 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_03.js
@@ -0,0 +1,203 @@
+Grailbird.data.tweets_2019_03 = 
+[
+	{
+		"created_at": "Mon Mar 04 18:00:32 +0000 2019",
+		"id": 1102629914757550100,
+		"id_str": "1102629914757550080",
+		"full_text": "As we prepare for #NASPA19 in Los Angeles, take a look back at the 1980 @NASPAtweets  program covers. The theme was Higher Education on No Frills: Strategies for Change. #NASPA100 https://t.co/hijWTFBU7P",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			179
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NASPA19",
+					"indices": [
+						18,
+						26
+					]
+				},
+				{
+					"text": "NASPA100",
+					"indices": [
+						170,
+						179
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						72,
+						84
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1102629913100779500,
+					"id_str": "1102629913100779525",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/D01UEGEXgAUQ0zB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D01UEGEXgAUQ0zB.jpg",
+					"url": "https://t.co/hijWTFBU7P",
+					"display_url": "pic.twitter.com/hijWTFBU7P",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1102629914757550080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 309,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 309,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 309,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1102629913100779500,
+					"id_str": "1102629913100779525",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/D01UEGEXgAUQ0zB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D01UEGEXgAUQ0zB.jpg",
+					"url": "https://t.co/hijWTFBU7P",
+					"display_url": "pic.twitter.com/hijWTFBU7P",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1102629914757550080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 309,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 309,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 309,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 160,
+			"friends_count": 23,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 43,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 294,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "As we prepare for #NASPA19 in Los Angeles, take a look back at the 1980 @NASPAtweets  program covers. The theme was Higher Education on No Frills: Strategies for Change. #NASPA100 https://t.co/hijWTFBU7P"
+	}
+]
