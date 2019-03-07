6b52938ebd039e5ecd7866300816f9a99f8fd67b
drjwlowery <jwlowery@gmail.com>
2019-03-06 20:17:50 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 322,
-	"created_at": "Wed Mar 06 01:17:49 +0000 2019",
+	"tweets": 323,
+	"created_at": "Thu Mar 07 01:17:49 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 06 20:00:57 +0000 2019",
+		"id": 1103384995899629600,
+		"id_str": "1103384995899629569",
+		"full_text": "While  @365TPE is just beginning, @NASPAtweets has offered a placement service since 1935.  Richard Hulet, Dean of Students at @IllinoisStateU  described the placement service in 1965.  Former coordinators included Don Gardner, Fred Turner, and Arno Nowotny #TPE #NASPA19 https://t.co/Gp8tXFqpLt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TPE",
+					"indices": [
+						258,
+						262
+					]
+				},
+				{
+					"text": "NASPA19",
+					"indices": [
+						263,
+						271
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "365TPE",
+					"name": "ThePlacementExchange",
+					"id": 69670200,
+					"id_str": "69670200",
+					"indices": [
+						7,
+						14
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						34,
+						46
+					]
+				},
+				{
+					"screen_name": "IllinoisStateU",
+					"name": "Illinois State",
+					"id": 41873654,
+					"id_str": "41873654",
+					"indices": [
+						127,
+						142
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1103384994431598600,
+					"id_str": "1103384994431598593",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/D1ACznOXgAE_0fP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1ACznOXgAE_0fP.jpg",
+					"url": "https://t.co/Gp8tXFqpLt",
+					"display_url": "pic.twitter.com/Gp8tXFqpLt",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1103384995899629569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 145,
+							"h": 145,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 335,
+							"h": 145,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 335,
+							"h": 145,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 335,
+							"h": 145,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1103384994431598600,
+					"id_str": "1103384994431598593",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/D1ACznOXgAE_0fP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1ACznOXgAE_0fP.jpg",
+					"url": "https://t.co/Gp8tXFqpLt",
+					"display_url": "pic.twitter.com/Gp8tXFqpLt",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1103384995899629569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 145,
+							"h": 145,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 335,
+							"h": 145,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 335,
+							"h": 145,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 335,
+							"h": 145,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 162,
+			"friends_count": 23,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 44,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 296,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "While  @365TPE is just beginning, @NASPAtweets has offered a placement service since 1935.  Richard Hulet, Dean of Students at @IllinoisStateU  described the placement service in 1965.  Former coordinators included Don Gardner, Fred Turner, and Arno Nowotny #TPE #NASPA19 https://t.co/Gp8tXFqpLt"
+	},
+	{
 		"created_at": "Tue Mar 05 17:01:05 +0000 2019",
 		"id": 1102977343793098800,
 		"id_str": "1102977343793098757",
