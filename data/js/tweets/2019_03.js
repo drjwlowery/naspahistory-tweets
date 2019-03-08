fd3937adb6b801d38433ed009f6e307f2be8a7a5
drjwlowery <jwlowery@gmail.com>
2019-03-07 20:17:50 -0500

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Thu Mar 07 21:05:11 +0000 2019",
+		"id": 1103763548944166900,
+		"id_str": "1103763548944166912",
+		"full_text": "The Placement Services offered by @NASPAtweets grew over time. Here we can see the Placement Service at the 1975 NASPA Conference held in San Francisco. @365TPE #NASPA19 https://t.co/udw4Xxe8iE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NASPA19",
+					"indices": [
+						161,
+						169
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						34,
+						46
+					]
+				},
+				{
+					"screen_name": "365TPE",
+					"name": "ThePlacementExchange",
+					"id": 69670200,
+					"id_str": "69670200",
+					"indices": [
+						153,
+						160
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1103763547597795300,
+					"id_str": "1103763547597795332",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/D1FbGTqXcAQbXhy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1FbGTqXcAQbXhy.jpg",
+					"url": "https://t.co/udw4Xxe8iE",
+					"display_url": "pic.twitter.com/udw4Xxe8iE",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1103763548944166912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 450,
+							"h": 234,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 450,
+							"h": 234,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 450,
+							"h": 234,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1103763547597795300,
+					"id_str": "1103763547597795332",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/D1FbGTqXcAQbXhy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1FbGTqXcAQbXhy.jpg",
+					"url": "https://t.co/udw4Xxe8iE",
+					"display_url": "pic.twitter.com/udw4Xxe8iE",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1103763548944166912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 450,
+							"h": 234,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 450,
+							"h": 234,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 450,
+							"h": 234,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 163,
+			"friends_count": 23,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 44,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 298,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The Placement Services offered by @NASPAtweets grew over time. Here we can see the Placement Service at the 1975 NASPA Conference held in San Francisco. @365TPE #NASPA19 https://t.co/udw4Xxe8iE"
+	},
+	{
 		"created_at": "Wed Mar 06 20:00:57 +0000 2019",
 		"id": 1103384995899629600,
 		"id_str": "1103384995899629569",
