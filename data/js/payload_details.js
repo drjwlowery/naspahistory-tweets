d472f1e340c5cf3f259e2631dc486ba4a9e31f50
drjwlowery <jwlowery@gmail.com>
2019-04-29 21:17:51 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 377,
-	"created_at": "Sat Apr 20 01:17:49 +0000 2019",
+	"tweets": 378,
+	"created_at": "Tue Apr 30 01:17:49 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 29 18:10:15 +0000 2019",
+		"id": 1122926083131752400,
+		"id_str": "1122926083131752448",
+		"full_text": "In 1938, @NASPAtweets met at @UWMadison from April 27-30. Donfred Gardner was NADAM President. This was a unique conference because NADAM invited the National Interfraternity Council and its member organizations to send representatives. This doubled the attendance to 164. https://t.co/bkLNYpQlJ3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						9,
+						21
+					]
+				},
+				{
+					"screen_name": "UWMadison",
+					"name": "UW-Madison",
+					"id": 15763818,
+					"id_str": "15763818",
+					"indices": [
+						29,
+						39
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1122926081839915000,
+					"id_str": "1122926081839915010",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/D5VvUmkW4AImknG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5VvUmkW4AImknG.jpg",
+					"url": "https://t.co/bkLNYpQlJ3",
+					"display_url": "pic.twitter.com/bkLNYpQlJ3",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1122926083131752448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 570,
+							"h": 349,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 570,
+							"h": 349,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 570,
+							"h": 349,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1122926081839915000,
+					"id_str": "1122926081839915010",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/D5VvUmkW4AImknG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5VvUmkW4AImknG.jpg",
+					"url": "https://t.co/bkLNYpQlJ3",
+					"display_url": "pic.twitter.com/bkLNYpQlJ3",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1122926083131752448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 570,
+							"h": 349,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 570,
+							"h": 349,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 570,
+							"h": 349,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 207,
+			"friends_count": 23,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 55,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 359,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In 1938, @NASPAtweets met at @UWMadison from April 27-30. Donfred Gardner was NADAM President. This was a unique conference because NADAM invited the National Interfraternity Council and its member organizations to send representatives. This doubled the attendance to 164. https://t.co/bkLNYpQlJ3"
+	},
+	{
 		"created_at": "Fri Apr 19 15:45:22 +0000 2019",
 		"id": 1119265742594150400,
 		"id_str": "1119265742594150400",
