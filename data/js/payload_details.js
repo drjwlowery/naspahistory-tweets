6b782b0beefd05d160869334a55e23a6187e60b7
drjwlowery <jwlowery@gmail.com>
2019-05-16 21:17:50 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 388,
-	"created_at": "Wed May 15 01:17:49 +0000 2019",
+	"tweets": 389,
+	"created_at": "Fri May 17 01:17:49 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 15 15:02:35 +0000 2019",
+		"id": 1128677060942151700,
+		"id_str": "1128677060942151682",
+		"full_text": "At the 1926 @NASPAtweets conference, the attendees discussed \"The Liquor Problem.\" Dean Thomas Arkle Clark of @Illinois_Alma expressed the concern that while before the war students hid their drinking, in 1926 \"they were proud of it and like to be thought sophisticated.\" https://t.co/UMVVFxVoyt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						12,
+						24
+					]
+				},
+				{
+					"screen_name": "Illinois_Alma",
+					"name": "University of Illinois",
+					"id": 18165866,
+					"id_str": "18165866",
+					"indices": [
+						110,
+						124
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1128677059314769900,
+					"id_str": "1128677059314769921",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/D6ndzjKW4AET_5v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6ndzjKW4AET_5v.jpg",
+					"url": "https://t.co/UMVVFxVoyt",
+					"display_url": "pic.twitter.com/UMVVFxVoyt",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1128677060942151682/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1150,
+							"h": 721,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1150,
+							"h": 721,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1128677059314769900,
+					"id_str": "1128677059314769921",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/D6ndzjKW4AET_5v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6ndzjKW4AET_5v.jpg",
+					"url": "https://t.co/UMVVFxVoyt",
+					"display_url": "pic.twitter.com/UMVVFxVoyt",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1128677060942151682/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1150,
+							"h": 721,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1150,
+							"h": 721,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 215,
+			"friends_count": 23,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 55,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 373,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "At the 1926 @NASPAtweets conference, the attendees discussed \"The Liquor Problem.\" Dean Thomas Arkle Clark of @Illinois_Alma expressed the concern that while before the war students hid their drinking, in 1926 \"they were proud of it and like to be thought sophisticated.\" https://t.co/UMVVFxVoyt"
+	},
+	{
 		"created_at": "Tue May 14 16:30:50 +0000 2019",
 		"id": 1128336880729239600,
 		"id_str": "1128336880729239559",
