1e2354f859122c142691ba5ec1031e8f7450798e
drjwlowery <jwlowery@gmail.com>
2019-06-04 21:17:50 -0400

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 389,
-	"created_at": "Fri May 17 01:17:49 +0000 2019",
+	"tweets": 391,
+	"created_at": "Wed Jun 05 01:17:49 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_06.js
@@ -0,0 +1,345 @@
+Grailbird.data.tweets_2019_06 = 
+[
+	{
+		"created_at": "Tue Jun 04 14:36:45 +0000 2019",
+		"id": 1135918314310447100,
+		"id_str": "1135918314310447106",
+		"full_text": "@NorthwesternU Read @NorthwesternU's President Morton Schapiro's message to the campus community about Patricia Telles-Irvin's passing. https://t.co/MuzdLOuoqA",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			159
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NorthwesternU",
+					"name": "Northwestern",
+					"id": 33639255,
+					"id_str": "33639255",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "NorthwesternU",
+					"name": "Northwestern",
+					"id": 33639255,
+					"id_str": "33639255",
+					"indices": [
+						20,
+						34
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/MuzdLOuoqA",
+					"expanded_url": "https://news.northwestern.edu/stories/2019/06/vice-president-patricia-telles-irvin-has-passed-away",
+					"display_url": "news.northwestern.edu/stories/2019/0…",
+					"indices": [
+						136,
+						159
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": 1135917518407651300,
+		"in_reply_to_status_id_str": "1135917518407651329",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 214,
+			"friends_count": 23,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 55,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 376,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@NorthwesternU Read @NorthwesternU's President Morton Schapiro's message to the campus community about Patricia Telles-Irvin's passing. https://t.co/MuzdLOuoqA"
+	},
+	{
+		"created_at": "Tue Jun 04 14:33:35 +0000 2019",
+		"id": 1135917518407651300,
+		"id_str": "1135917518407651329",
+		"full_text": "@NorthwesternU @NASPAtweets @UF Patricia Telles-Irvin was the first Hispanic @NASPAtweets president.\n\nHer annual conference was held in Phoenix, Arizona and the theme was Ignite Leadership Influence Change. https://t.co/9XkB19XZ4p",
+		"truncated": false,
+		"display_text_range": [
+			32,
+			206
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NorthwesternU",
+					"name": "Northwestern",
+					"id": 33639255,
+					"id_str": "33639255",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						15,
+						27
+					]
+				},
+				{
+					"screen_name": "UF",
+					"name": "FLORIDA",
+					"id": 44168607,
+					"id_str": "44168607",
+					"indices": [
+						28,
+						31
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						77,
+						89
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1135917516725686300,
+					"id_str": "1135917516725686273",
+					"indices": [
+						207,
+						230
+					],
+					"media_url": "http://pbs.twimg.com/media/D8OW9o6UEAEhSs5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8OW9o6UEAEhSs5.jpg",
+					"url": "https://t.co/9XkB19XZ4p",
+					"display_url": "pic.twitter.com/9XkB19XZ4p",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1135917518407651329/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 872,
+							"h": 256,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 872,
+							"h": 256,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1135917516725686300,
+					"id_str": "1135917516725686273",
+					"indices": [
+						207,
+						230
+					],
+					"media_url": "http://pbs.twimg.com/media/D8OW9o6UEAEhSs5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8OW9o6UEAEhSs5.jpg",
+					"url": "https://t.co/9XkB19XZ4p",
+					"display_url": "pic.twitter.com/9XkB19XZ4p",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1135917518407651329/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 872,
+							"h": 256,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 872,
+							"h": 256,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": 1135915991399948300,
+		"in_reply_to_status_id_str": "1135915991399948290",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 214,
+			"friends_count": 23,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 55,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 376,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@NorthwesternU @NASPAtweets @UF Patricia Telles-Irvin was the first Hispanic @NASPAtweets president.\n\nHer annual conference was held in Phoenix, Arizona and the theme was Ignite Leadership Influence Change. https://t.co/9XkB19XZ4p"
+	}
+]
