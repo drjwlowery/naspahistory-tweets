6c9b5cce0ee547c84ff44cdbebe5646ad32cfb99
drjwlowery <jwlowery@gmail.com>
2019-10-28 21:17:50 -0400

    Added 1 tweets

--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Mon Oct 28 17:19:00 +0000 2019",
+		"id": 1188867774820434000,
+		"id_str": "1188867774820433920",
+		"full_text": "The NASPA Foundation's  Distinguished Pillar award is named for former @NASPAtweets president, John L. Blackburn. Blackburn worked in student affairs at @UofAlabama from 1956-1969, the @UofDenver from 1969 until 1978, and then returned to @UofAlabama. He died in 2009.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			268
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						71,
+						83
+					]
+				},
+				{
+					"screen_name": "UofAlabama",
+					"name": "The Univ. of Alabama",
+					"id": 18685948,
+					"id_str": "18685948",
+					"indices": [
+						153,
+						164
+					]
+				},
+				{
+					"screen_name": "UofDenver",
+					"name": "University of Denver",
+					"id": 17349956,
+					"id_str": "17349956",
+					"indices": [
+						185,
+						195
+					]
+				},
+				{
+					"screen_name": "UofAlabama",
+					"name": "The Univ. of Alabama",
+					"id": 18685948,
+					"id_str": "18685948",
+					"indices": [
+						239,
+						250
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 226,
+			"friends_count": 23,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 56,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 392,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "The NASPA Foundation's  Distinguished Pillar award is named for former @NASPAtweets president, John L. Blackburn. Blackburn worked in student affairs at @UofAlabama from 1956-1969, the @UofDenver from 1969 until 1978, and then returned to @UofAlabama. He died in 2009."
+	},
+	{
 		"created_at": "Wed Oct 23 17:35:56 +0000 2019",
 		"id": 1187060099816329200,
 		"id_str": "1187060099816329216",
