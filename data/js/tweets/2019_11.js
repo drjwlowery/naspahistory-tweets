353edacd78f4a7012dc2b91ff3ec21b3b38b947f
drjwlowery <jwlowery@gmail.com>
2019-11-05 20:17:50 -0500

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,297 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Tue Nov 05 18:04:00 +0000 2019",
+		"id": 1191778202659766300,
+		"id_str": "1191778202659766273",
+		"full_text": "@NASPAtweets @OhioState Dr. Cowley traced the history of the Dean of Men position in his address. He identified LeBaron Russell Briggs as the first true dean of men and first person to be responsible for \"student relations as separate and distinct from instruction\" (p. 88). https://t.co/oWGdJ7xk65 https://t.co/1TNbczqYKM",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			298
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "OhioState",
+					"name": "Ohio State",
+					"id": 18846918,
+					"id_str": "18846918",
+					"indices": [
+						13,
+						23
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/oWGdJ7xk65",
+					"expanded_url": "https://babel.hathitrust.org/cgi/pt?id=uc1.b2863300&view=1up&seq=104",
+					"display_url": "babel.hathitrust.org/cgi/pt?id=uc1.…",
+					"indices": [
+						275,
+						298
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1190429249779511300,
+					"id_str": "1190429249779511296",
+					"indices": [
+						299,
+						322
+					],
+					"media_url": "http://pbs.twimg.com/media/EIVBGHNXsAASSn7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIVBGHNXsAASSn7.jpg",
+					"url": "https://t.co/1TNbczqYKM",
+					"display_url": "pic.twitter.com/1TNbczqYKM",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1191778202659766273/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 821,
+							"h": 411,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 821,
+							"h": 411,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 394,
+									"y": 80,
+									"h": 204,
+									"w": 204
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 326,
+									"y": 66,
+									"h": 168,
+									"w": 168
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 394,
+									"y": 80,
+									"h": 204,
+									"w": 204
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 394,
+									"y": 80,
+									"h": 204,
+									"w": 204
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1190429249779511300,
+					"id_str": "1190429249779511296",
+					"indices": [
+						299,
+						322
+					],
+					"media_url": "http://pbs.twimg.com/media/EIVBGHNXsAASSn7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIVBGHNXsAASSn7.jpg",
+					"url": "https://t.co/1TNbczqYKM",
+					"display_url": "pic.twitter.com/1TNbczqYKM",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1191778202659766273/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 821,
+							"h": 411,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 821,
+							"h": 411,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 394,
+									"y": 80,
+									"h": 204,
+									"w": 204
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 326,
+									"y": 66,
+									"h": 168,
+									"w": 168
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 394,
+									"y": 80,
+									"h": 204,
+									"w": 204
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 394,
+									"y": 80,
+									"h": 204,
+									"w": 204
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1190270508119531500,
+		"in_reply_to_status_id_str": "1190270508119531522",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 226,
+			"friends_count": 23,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 56,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 399,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@NASPAtweets @OhioState Dr. Cowley traced the history of the Dean of Men position in his address. He identified LeBaron Russell Briggs as the first true dean of men and first person to be responsible for \"student relations as separate and distinct from instruction\" (p. 88). https://t.co/oWGdJ7xk65 https://t.co/1TNbczqYKM"
+	},
+	{
 		"created_at": "Mon Nov 04 18:56:00 +0000 2019",
 		"id": 1191428900456263700,
 		"id_str": "1191428900456263690",
