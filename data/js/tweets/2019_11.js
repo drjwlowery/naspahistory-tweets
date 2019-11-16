62d565e5c0caab996296abda6bc84773fff07122
drjwlowery <jwlowery@gmail.com>
2019-11-15 20:17:50 -0500

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,325 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 15 18:01:00 +0000 2019",
+		"id": 1195401326210908200,
+		"id_str": "1195401326210908160",
+		"full_text": "Dean Robert Rienow of @uiowa saw greater value in training than some of his peers. He concluded, \"I do not think it is entirely true that Deans of Men are just born, and not made. I think, while they are born, they can be made better.\" (1931, p. 113) https://t.co/EMwYU8SZhS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			250
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "uiowa",
+					"name": "University of Iowa",
+					"id": 14534931,
+					"id_str": "14534931",
+					"indices": [
+						22,
+						28
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1192859683314380800,
+					"id_str": "1192859683314380800",
+					"indices": [
+						251,
+						274
+					],
+					"media_url": "http://pbs.twimg.com/media/EI3jj9rWsAAgcHt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EI3jj9rWsAAgcHt.jpg",
+					"url": "https://t.co/EMwYU8SZhS",
+					"display_url": "pic.twitter.com/EMwYU8SZhS",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1195401326210908160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 389,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 959,
+							"h": 548,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 959,
+							"h": 548,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 237,
+									"y": 6,
+									"h": 51,
+									"w": 51
+								},
+								{
+									"x": 713,
+									"y": 174,
+									"h": 166,
+									"w": 166
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 168,
+									"y": 4,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 505,
+									"y": 123,
+									"h": 117,
+									"w": 117
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 237,
+									"y": 6,
+									"h": 51,
+									"w": 51
+								},
+								{
+									"x": 713,
+									"y": 174,
+									"h": 166,
+									"w": 166
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 237,
+									"y": 6,
+									"h": 51,
+									"w": 51
+								},
+								{
+									"x": 713,
+									"y": 174,
+									"h": 166,
+									"w": 166
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1192859683314380800,
+					"id_str": "1192859683314380800",
+					"indices": [
+						251,
+						274
+					],
+					"media_url": "http://pbs.twimg.com/media/EI3jj9rWsAAgcHt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EI3jj9rWsAAgcHt.jpg",
+					"url": "https://t.co/EMwYU8SZhS",
+					"display_url": "pic.twitter.com/EMwYU8SZhS",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1195401326210908160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 389,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 959,
+							"h": 548,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 959,
+							"h": 548,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 237,
+									"y": 6,
+									"h": 51,
+									"w": 51
+								},
+								{
+									"x": 713,
+									"y": 174,
+									"h": 166,
+									"w": 166
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 168,
+									"y": 4,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 505,
+									"y": 123,
+									"h": 117,
+									"w": 117
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 237,
+									"y": 6,
+									"h": 51,
+									"w": 51
+								},
+								{
+									"x": 713,
+									"y": 174,
+									"h": 166,
+									"w": 166
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 237,
+									"y": 6,
+									"h": 51,
+									"w": 51
+								},
+								{
+									"x": 713,
+									"y": 174,
+									"h": 166,
+									"w": 166
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 228,
+			"friends_count": 23,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 57,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 409,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dean Robert Rienow of @uiowa saw greater value in training than some of his peers. He concluded, \"I do not think it is entirely true that Deans of Men are just born, and not made. I think, while they are born, they can be made better.\" (1931, p. 113) https://t.co/EMwYU8SZhS"
+	},
+	{
 		"created_at": "Thu Nov 14 17:55:00 +0000 2019",
 		"id": 1195037427967987700,
 		"id_str": "1195037427967987712",
