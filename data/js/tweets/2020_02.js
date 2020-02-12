1def8c30179c53f34b7dfa32c20b89bd0198085a
drjwlowery <jwlowery@gmail.com>
2020-02-11 20:17:50 -0500

    Added 1 tweets

--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Tue Feb 11 22:02:37 +0000 2020",
+		"id": 1227352264756600800,
+		"id_str": "1227352264756600833",
+		"full_text": "Congratulations to Dr. Karen Pennington who is one of @NASPAtweets 2019-2020 recipients of the Scott Goodnight Award for Outstanding Performance as a Dean.\nDr. Karen L. Pennington is the Vice President for Student Development and Campus Life at @montclairstateu. https://t.co/XDHwtgbWaY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			262
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						54,
+						66
+					]
+				},
+				{
+					"screen_name": "montclairstateu",
+					"name": "Montclair State U",
+					"id": 47703620,
+					"id_str": "47703620",
+					"indices": [
+						245,
+						261
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1227352154559713300,
+					"id_str": "1227352154559713282",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/EQhuSCLXkAIEufB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQhuSCLXkAIEufB.jpg",
+					"url": "https://t.co/XDHwtgbWaY",
+					"display_url": "pic.twitter.com/XDHwtgbWaY",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1227352264756600833/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 675,
+							"h": 465,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 465,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 675,
+							"h": 465,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1227352154559713300,
+					"id_str": "1227352154559713282",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/EQhuSCLXkAIEufB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQhuSCLXkAIEufB.jpg",
+					"url": "https://t.co/XDHwtgbWaY",
+					"display_url": "pic.twitter.com/XDHwtgbWaY",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1227352264756600833/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 675,
+							"h": 465,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 465,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 675,
+							"h": 465,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 230,
+			"friends_count": 26,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 61,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 419,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Congratulations to Dr. Karen Pennington who is one of @NASPAtweets 2019-2020 recipients of the Scott Goodnight Award for Outstanding Performance as a Dean.\nDr. Karen L. Pennington is the Vice President for Student Development and Campus Life at @montclairstateu. https://t.co/XDHwtgbWaY"
+	},
+	{
 		"created_at": "Tue Feb 04 17:46:53 +0000 2020",
 		"id": 1224751189562183700,
 		"id_str": "1224751189562183680",
