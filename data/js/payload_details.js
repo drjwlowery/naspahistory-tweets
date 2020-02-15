c06d1ccf8ff3588f6b7b827372a351c1d137fa57
drjwlowery <jwlowery@gmail.com>
2020-02-14 20:17:51 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 428,
-	"created_at": "Fri Feb 14 01:17:49 +0000 2020",
+	"tweets": 429,
+	"created_at": "Sat Feb 15 01:17:50 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Fri Feb 14 19:41:00 +0000 2020",
+		"id": 1228403787439624200,
+		"id_str": "1228403787439624192",
+		"full_text": "Robert Schwartz also discusses Scoot Goodnight extensively in his book, Deans of Mean and the Shaping of Modern College Culture. https://t.co/wQVRusOn6D",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1227356952864477200,
+					"id_str": "1227356952864477184",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/EQhypVQX0AAstu8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQhypVQX0AAstu8.jpg",
+					"url": "https://t.co/wQVRusOn6D",
+					"display_url": "pic.twitter.com/wQVRusOn6D",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1228403787439624192/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 475,
+							"h": 475,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 475,
+							"h": 475,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 475,
+							"h": 475,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1227356952864477200,
+					"id_str": "1227356952864477184",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/EQhypVQX0AAstu8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQhypVQX0AAstu8.jpg",
+					"url": "https://t.co/wQVRusOn6D",
+					"display_url": "pic.twitter.com/wQVRusOn6D",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1228403787439624192/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 475,
+							"h": 475,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 475,
+							"h": 475,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 475,
+							"h": 475,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 230,
+			"friends_count": 26,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 61,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 423,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Robert Schwartz also discusses Scoot Goodnight extensively in his book, Deans of Mean and the Shaping of Modern College Culture. https://t.co/wQVRusOn6D"
+	},
+	{
 		"created_at": "Thu Feb 13 19:33:00 +0000 2020",
 		"id": 1228039386589212700,
 		"id_str": "1228039386589212673",
