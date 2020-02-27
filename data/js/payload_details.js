442fb185b048daff20ca0d5a7f1ccd2100dc916e
drjwlowery <jwlowery@gmail.com>
2020-02-26 20:17:50 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 434,
-	"created_at": "Wed Feb 26 01:17:51 +0000 2020",
+	"tweets": 435,
+	"created_at": "Thu Feb 27 01:17:49 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Wed Feb 26 18:03:00 +0000 2020",
+		"id": 1232727779528962000,
+		"id_str": "1232727779528962048",
+		"full_text": "Over the course of his career, Dr. Gehring was @naspatweets by NASPA for his contributions:\n\n1985 Outstanding Contribution to Literature or Research Award, \n\n1990 Shaffer Award for Academic Excellence as a Graduate Faculty Member, and\n\n2001 Pillar of the Profession. https://t.co/g7pHRGztIg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			266
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						47,
+						59
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1232420785383526400,
+					"id_str": "1232420785383526401",
+					"indices": [
+						267,
+						290
+					],
+					"media_url": "http://pbs.twimg.com/media/ERpwLLaX0AEy6k3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERpwLLaX0AEy6k3.jpg",
+					"url": "https://t.co/g7pHRGztIg",
+					"display_url": "pic.twitter.com/g7pHRGztIg",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1232727779528962048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 631,
+							"h": 812,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 631,
+							"h": 812,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 528,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1232420785383526400,
+					"id_str": "1232420785383526401",
+					"indices": [
+						267,
+						290
+					],
+					"media_url": "http://pbs.twimg.com/media/ERpwLLaX0AEy6k3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERpwLLaX0AEy6k3.jpg",
+					"url": "https://t.co/g7pHRGztIg",
+					"display_url": "pic.twitter.com/g7pHRGztIg",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1232727779528962048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 631,
+							"h": 812,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 631,
+							"h": 812,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 528,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1232420258503417900,
+		"in_reply_to_status_id_str": "1232420258503417856",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 231,
+			"friends_count": 26,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 61,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 431,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Over the course of his career, Dr. Gehring was @naspatweets by NASPA for his contributions:\n\n1985 Outstanding Contribution to Literature or Research Award, \n\n1990 Shaffer Award for Academic Excellence as a Graduate Faculty Member, and\n\n2001 Pillar of the Profession. https://t.co/g7pHRGztIg"
+	},
+	{
 		"created_at": "Tue Feb 25 21:41:01 +0000 2020",
 		"id": 1232420258503417900,
 		"id_str": "1232420258503417856",
