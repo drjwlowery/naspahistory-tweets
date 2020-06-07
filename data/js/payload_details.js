a8e095af57c493c1967121c67ae3223a295e5967
drjwlowery <jwlowery@gmail.com>
2020-06-06 21:17:51 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 473,
-	"created_at": "Wed May 13 01:17:49 +0000 2020",
+	"tweets": 474,
+	"created_at": "Sun Jun 07 01:17:50 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_06.js
@@ -0,0 +1,208 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Sat Jun 06 17:50:00 +0000 2020",
+		"id": 1269325682372423700,
+		"id_str": "1269325682372423683",
+		"full_text": "On June 6, 1943, Dr. V. I. Moore passed away. He served as Dean of Student Life at @UTAustin from 1927 until his death. He was the 1931-1932 president of @NASPAtweets.\n\nAt the 1944 conference in Chicago, NADAM passed a memorial resolution. Read it here: https://t.co/HvjbMtXcjc https://t.co/eKcNUe47nN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UTAustin",
+					"name": "UT Austin",
+					"id": 15637375,
+					"id_str": "15637375",
+					"indices": [
+						83,
+						92
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						154,
+						166
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/HvjbMtXcjc",
+					"expanded_url": "https://itty.bitty.site/#Memorial_Resolution_for_Dr._V._I._Moore/?XQAAAALLDAAAAAAAAAAeHAAGRVIW7YmVEWn8yBxahqtCRTPzxYjNT9Ay/GNglUEHays6CUMmDM9yCpgzjLyXc/kPctpMO+sl2pF0cDHNxC7+qjDgQ/zhuaYgsjwZ2ixi1mW4oSMBUuI0S55fz2zh05w6pBoPwtLpBRjc/8CVLNvSuMXOuQCOqSDt80VfwQZMUEz/yb5o4d+xvofwgN4mL9m0tkbC82/NQagg1INN8tGcXgSETM0H0YPe8ovkXaQYQ+gHbeahmZvC8ht1072yoHySiuJJBkXedocrT7ZYo2W/IlaCOJfuXIiz21NrKKBeHERuwNTYsETHuBk3jd7chnPu1SiFLuYfGEKDYfEMPMwQsgsiPad6FgLWy/SMg501jTQyZPwv9sf1E0Q8hzZdY6VQRrOY1YeVblvRQtuNiAmQ7WYvnx7rG7o6t+S8ohdPcs2H6PLaCZM5cfStq88FcB5TYkgLZUWFaVmH1d5bBd2g7xgwsu8ny3xgVBhYyhB2ymBkBEK2RrfJ4KiK0skpuOI3furBMQqlXx77Aq1TT8l0lABi+IrxRacXbNztTYdvBkFumS745VEsVKvD4eAKBARZ71CWtd/Dve/Xr8kYE6jjOc6FuxHEyY8Zymn6omWqI2k2yUykxFuPJ3HOlP8SJH1gIEwuzTRHjKEAdKJolrKJxZm7b3rA9HDampJHKqAiMJEM8CDRWX9ItVHXLirAd3Y6hOxmJEEAS2DpigyN0cuZdeY1g2udbMe428KB0kDeRNSD/o+0Jl+MAdwkgME0VTOp3g2179MsvG1mnEwXspHGqMwjLdRxqeBYJtB0gnEgV4cUg7sJ5LtMwHvg44PKYZHbIUqMf6+nho7EmKFA1kmw3kl12XBgIYyQcFNhuyj6jlOeU2Dfjz7fKF8OyA4w6P5x21WKzojh2ggMyPeJXnVeNWtJcdnRfKnF6rFuwyV2GLArTP3TZlAANMF/S4lgChfBIZc0/PLSpt6Wo1cwAizKd0jZwAuj0KYZ9yqoVNoQgya30UBk0BQI0PBEU8ZQpNS4dUFPIM/Eb1NaOTcFlkX8N++7vSCL4f6q2N3um+Ku9TilyIMB1zvvNIg6//nc3ic=",
+					"display_url": "itty.bitty.site/#Memorial_Reso…",
+					"indices": [
+						254,
+						277
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1241416708809732000,
+					"id_str": "1241416708809732103",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/ETpl61rWsAcz1yv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETpl61rWsAcz1yv.jpg",
+					"url": "https://t.co/eKcNUe47nN",
+					"display_url": "pic.twitter.com/eKcNUe47nN",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1269325682372423683/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 662,
+							"h": 422,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 662,
+							"h": 422,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 662,
+							"h": 422,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1241416708809732000,
+					"id_str": "1241416708809732103",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/ETpl61rWsAcz1yv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETpl61rWsAcz1yv.jpg",
+					"url": "https://t.co/eKcNUe47nN",
+					"display_url": "pic.twitter.com/eKcNUe47nN",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1269325682372423683/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 662,
+							"h": 422,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 662,
+							"h": 422,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 662,
+							"h": 422,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 242,
+			"friends_count": 26,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 64,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 486,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On June 6, 1943, Dr. V. I. Moore passed away. He served as Dean of Student Life at @UTAustin from 1927 until his death. He was the 1931-1932 president of @NASPAtweets.\n\nAt the 1944 conference in Chicago, NADAM passed a memorial resolution. Read it here: https://t.co/HvjbMtXcjc https://t.co/eKcNUe47nN"
+	}
+]
