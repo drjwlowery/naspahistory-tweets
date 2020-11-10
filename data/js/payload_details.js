93cdb5247c58bf9531870439fa631c5bfb31bbfa
drjwlowery <jwlowery@gmail.com>
2020-11-09 20:17:51 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 489,
-	"created_at": "Tue Nov 03 01:17:49 +0000 2020",
+	"tweets": 490,
+	"created_at": "Tue Nov 10 01:17:49 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Mon Nov 09 19:37:56 +0000 2020",
+		"id": 1325885351047598000,
+		"id_str": "1325885351047598087",
+		"full_text": "Dr. Jim Rhatigan was 1975-1976 @NASPAtweets President and served for many years as NASPA Historian too.\n\nIn this video, Dr. Rhatigan discusses \"Never Underestimate the Importance of an Ordinary Day\" as part of the @WSUFoundation Things I Believe series.\n\nhttps://t.co/X4HV8yx2V4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						31,
+						43
+					]
+				},
+				{
+					"screen_name": "WSUFoundation",
+					"name": "WSU Foundation",
+					"id": 41167679,
+					"id_str": "41167679",
+					"indices": [
+						214,
+						228
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/X4HV8yx2V4",
+					"expanded_url": "https://www.youtube.com/watch?v=kmpbXbzr6t8&feature=youtu.be",
+					"display_url": "youtube.com/watch?v=kmpbXb…",
+					"indices": [
+						255,
+						278
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 248,
+			"friends_count": 26,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 69,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 504,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dr. Jim Rhatigan was 1975-1976 @NASPAtweets President and served for many years as NASPA Historian too.\n\nIn this video, Dr. Rhatigan discusses \"Never Underestimate the Importance of an Ordinary Day\" as part of the @WSUFoundation Things I Believe series.\n\nhttps://t.co/X4HV8yx2V4"
+	},
+	{
 		"created_at": "Mon Nov 02 18:34:00 +0000 2020",
 		"id": 1323332547540078600,
 		"id_str": "1323332547540078592",
