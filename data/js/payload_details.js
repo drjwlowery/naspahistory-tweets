6a27a9f6777ae7ed5d87212cc9b65cc995eb3947
drjwlowery <jwlowery@gmail.com>
2020-12-15 20:17:50 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 493,
-	"created_at": "Tue Dec 15 01:17:50 +0000 2020",
+	"tweets": 494,
+	"created_at": "Wed Dec 16 01:17:49 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Tue Dec 15 18:13:25 +0000 2020",
+		"id": 1338910046319767600,
+		"id_str": "1338910046319767556",
+		"full_text": "On Dec. 21, 1918, Robert Rienow wrote Scott Goodnight to suggest  a conference for deans of men. Goodnight agreed &amp; began planning a conference at the University of Wisconsin on Jan. 24-25, 1920 hosted by Goodnight. \n\nRead Rienow's idea here: https://t.co/9pBBhNurUg https://t.co/8G3WB3YoP1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9pBBhNurUg",
+					"expanded_url": "https://tinyurl.com/ybu6mhv3",
+					"display_url": "tinyurl.com/ybu6mhv3",
+					"indices": [
+						247,
+						270
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1338907218624852000,
+					"id_str": "1338907218624851970",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/EpTBBYFXMAICP6A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EpTBBYFXMAICP6A.jpg",
+					"url": "https://t.co/8G3WB3YoP1",
+					"display_url": "pic.twitter.com/8G3WB3YoP1",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1338910046319767556/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 360,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 884,
+							"h": 468,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 884,
+							"h": 468,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1338907218624852000,
+					"id_str": "1338907218624851970",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/EpTBBYFXMAICP6A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EpTBBYFXMAICP6A.jpg",
+					"url": "https://t.co/8G3WB3YoP1",
+					"display_url": "pic.twitter.com/8G3WB3YoP1",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1338910046319767556/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 360,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 884,
+							"h": 468,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 884,
+							"h": 468,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 247,
+			"friends_count": 26,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 69,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 509,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On Dec. 21, 1918, Robert Rienow wrote Scott Goodnight to suggest  a conference for deans of men. Goodnight agreed &amp; began planning a conference at the University of Wisconsin on Jan. 24-25, 1920 hosted by Goodnight. \n\nRead Rienow's idea here: https://t.co/9pBBhNurUg https://t.co/8G3WB3YoP1"
+	},
+	{
 		"created_at": "Mon Dec 14 19:15:00 +0000 2020",
 		"id": 1338563155971215400,
 		"id_str": "1338563155971215362",
