c533e51797fb5b8ebfd378491d43ebaa8f858b9e
drjwlowery <jwlowery@gmail.com>
2021-01-08 20:17:51 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 497,
-	"created_at": "Sun Jan 03 01:17:49 +0000 2021",
+	"tweets": 498,
+	"created_at": "Sat Jan 09 01:17:49 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Fri Jan 08 18:39:00 +0000 2021",
+		"id": 1347613792402886700,
+		"id_str": "1347613792402886659",
+		"full_text": "On this day in 1972, Fred H. Weaver died in India. He served @NASPAtweets as 1961-1962 President. \n\nHe served as Dean of Students at the University of North Carolina, Chapel Hill (1946-61), and then worked for the University of North Carolina System until 1968. https://t.co/rgWjCzR72Z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			261
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						61,
+						73
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1329157326117462000,
+					"id_str": "1329157326117462019",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/EnIdi8HW4AMNn8w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnIdi8HW4AMNn8w.jpg",
+					"url": "https://t.co/rgWjCzR72Z",
+					"display_url": "pic.twitter.com/rgWjCzR72Z",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1347613792402886659/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 354,
+							"h": 533,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 354,
+							"h": 533,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 354,
+							"h": 533,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1329157326117462000,
+					"id_str": "1329157326117462019",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/EnIdi8HW4AMNn8w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnIdi8HW4AMNn8w.jpg",
+					"url": "https://t.co/rgWjCzR72Z",
+					"display_url": "pic.twitter.com/rgWjCzR72Z",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1347613792402886659/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 354,
+							"h": 533,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 354,
+							"h": 533,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 354,
+							"h": 533,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 246,
+			"friends_count": 26,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 69,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 514,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On this day in 1972, Fred H. Weaver died in India. He served @NASPAtweets as 1961-1962 President. \n\nHe served as Dean of Students at the University of North Carolina, Chapel Hill (1946-61), and then worked for the University of North Carolina System until 1968. https://t.co/rgWjCzR72Z"
+	},
+	{
 		"created_at": "Sat Jan 02 18:25:00 +0000 2021",
 		"id": 1345435942136541200,
 		"id_str": "1345435942136541186",
