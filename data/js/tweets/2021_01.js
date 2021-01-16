f39c30afaca92a2dfd66f913b29d6f6d6600bca8
drjwlowery <jwlowery@gmail.com>
2021-01-15 20:17:51 -0500

    Added 1 tweets

--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Fri Jan 15 18:00:01 +0000 2021",
+		"id": 1350140699421913000,
+		"id_str": "1350140699421913088",
+		"full_text": "On January 15, 1946, Robert Rienow passed away. He served as dean of men at @uiowa from 1916 until 1946. \n\nRienow was the 1924-1925 @NASPAtweets president and is credited with first suggesting that deans of men gather together to discuss \"their mutual problems.\" https://t.co/B1UtAIgSFf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			262
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "uiowa",
+					"name": "University of Iowa",
+					"id": 14534931,
+					"id_str": "14534931",
+					"indices": [
+						76,
+						82
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						132,
+						144
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1241825719841415200,
+					"id_str": "1241825719841415170",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/ETvZ6agXYAI0V5C.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETvZ6agXYAI0V5C.jpg",
+					"url": "https://t.co/B1UtAIgSFf",
+					"display_url": "pic.twitter.com/B1UtAIgSFf",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1350140699421913088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1241825719841415200,
+					"id_str": "1241825719841415170",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/ETvZ6agXYAI0V5C.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETvZ6agXYAI0V5C.jpg",
+					"url": "https://t.co/B1UtAIgSFf",
+					"display_url": "pic.twitter.com/B1UtAIgSFf",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1350140699421913088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 245,
+			"friends_count": 26,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 69,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 515,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On January 15, 1946, Robert Rienow passed away. He served as dean of men at @uiowa from 1916 until 1946. \n\nRienow was the 1924-1925 @NASPAtweets president and is credited with first suggesting that deans of men gather together to discuss \"their mutual problems.\" https://t.co/B1UtAIgSFf"
+	},
+	{
 		"created_at": "Fri Jan 08 18:39:00 +0000 2021",
 		"id": 1347613792402886700,
 		"id_str": "1347613792402886659",
