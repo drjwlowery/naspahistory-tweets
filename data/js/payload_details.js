99481c08bc33624c34b00cabf0179d6661f9c412
drjwlowery <jwlowery@gmail.com>
2021-02-08 20:17:50 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 509,
-	"created_at": "Fri Feb 05 01:17:49 +0000 2021",
+	"tweets": 510,
+	"created_at": "Tue Feb 09 01:17:49 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Mon Feb 08 19:19:00 +0000 2021",
+		"id": 1358857883719372800,
+		"id_str": "1358857883719372801",
+		"full_text": "Dr. Lambert Seymour Corbett passed away on February 8, 1945. He served as Dean of Men at @UMaine from 1929 until 1945 as well as serving as a Professor of Animal Industry. In 1947, Corbett Hall was named in his memory.\n\nHe was the 1941-1942 president of @NASPAtweets. https://t.co/DfGnpOSHrE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			267
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UMaine",
+					"name": "University of Maine",
+					"id": 36454943,
+					"id_str": "36454943",
+					"indices": [
+						89,
+						96
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						254,
+						266
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1241831766488502300,
+					"id_str": "1241831766488502273",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/ETvfaYBX0AEB62c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETvfaYBX0AEB62c.jpg",
+					"url": "https://t.co/DfGnpOSHrE",
+					"display_url": "pic.twitter.com/DfGnpOSHrE",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1358857883719372801/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1241831766488502300,
+					"id_str": "1241831766488502273",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/ETvfaYBX0AEB62c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETvfaYBX0AEB62c.jpg",
+					"url": "https://t.co/DfGnpOSHrE",
+					"display_url": "pic.twitter.com/DfGnpOSHrE",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1358857883719372801/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 631,
+							"h": 256,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 248,
+			"friends_count": 26,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 70,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 529,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dr. Lambert Seymour Corbett passed away on February 8, 1945. He served as Dean of Men at @UMaine from 1929 until 1945 as well as serving as a Professor of Animal Industry. In 1947, Corbett Hall was named in his memory.\n\nHe was the 1941-1942 president of @NASPAtweets. https://t.co/DfGnpOSHrE"
+	},
+	{
 		"created_at": "Thu Feb 04 18:01:28 +0000 2021",
 		"id": 1357388821957116000,
 		"id_str": "1357388821957115905",
