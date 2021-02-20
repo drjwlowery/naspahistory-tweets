f17c003720e0d87eb9612105e547540df31be32f
drjwlowery <jwlowery@gmail.com>
2021-02-19 20:17:51 -0500

    Added 2 tweets

--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,350 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Fri Feb 19 18:33:00 +0000 2021",
+		"id": 1362832573144080400,
+		"id_str": "1362832573144080391",
+		"full_text": "Hocutt was named the first Dean of Students at @UDelaware in 1952. He was also the university's the first Vice President for Student Affairs. He retired in 1978.\n\nHe earned his bachelors degree at @williamandmary and later served as Dean of Men. https://t.co/7TF0vCVp7o",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			245
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UDelaware",
+					"name": "Univ. of Delaware",
+					"id": 29447487,
+					"id_str": "29447487",
+					"indices": [
+						47,
+						57
+					]
+				},
+				{
+					"screen_name": "williamandmary",
+					"name": "William & Mary",
+					"id": 15768704,
+					"id_str": "15768704",
+					"indices": [
+						197,
+						212
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1362812425712631800,
+					"id_str": "1362812425712631818",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/media/EumurkIXAAoXTiq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EumurkIXAAoXTiq.jpg",
+					"url": "https://t.co/7TF0vCVp7o",
+					"display_url": "pic.twitter.com/7TF0vCVp7o",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1362832573144080391/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 887,
+							"h": 570,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 887,
+							"h": 570,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 437,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1362812425712631800,
+					"id_str": "1362812425712631818",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/media/EumurkIXAAoXTiq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EumurkIXAAoXTiq.jpg",
+					"url": "https://t.co/7TF0vCVp7o",
+					"display_url": "pic.twitter.com/7TF0vCVp7o",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1362832573144080391/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 887,
+							"h": 570,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 887,
+							"h": 570,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 437,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 247,
+			"friends_count": 27,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 70,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 541,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hocutt was named the first Dean of Students at @UDelaware in 1952. He was also the university's the first Vice President for Student Affairs. He retired in 1978.\n\nHe earned his bachelors degree at @williamandmary and later served as Dean of Men. https://t.co/7TF0vCVp7o"
+	},
+	{
+		"created_at": "Fri Feb 19 17:11:26 +0000 2021",
+		"id": 1362812048875409400,
+		"id_str": "1362812048875409412",
+		"full_text": "@NASPAtweets @williamandmary @UDelaware Hocutt's service to @NASPAtweets was recognized in 1972 when he was one of the inaugural recipients of the Outstanding Service to NASPA Award which was later renamed the Fred Turner Award for Outstanding Service to NASPA. https://t.co/7ff6dZCKxl",
+		"truncated": false,
+		"display_text_range": [
+			40,
+			285
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "williamandmary",
+					"name": "William & Mary",
+					"id": 15768704,
+					"id_str": "15768704",
+					"indices": [
+						13,
+						28
+					]
+				},
+				{
+					"screen_name": "UDelaware",
+					"name": "Univ. of Delaware",
+					"id": 29447487,
+					"id_str": "29447487",
+					"indices": [
+						29,
+						39
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						60,
+						72
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/7ff6dZCKxl",
+					"expanded_url": "https://www.naspa.org/awards/fred-turner-award-for-outstanding-service-to-naspa4",
+					"display_url": "naspa.org/awards/fred-tu…",
+					"indices": [
+						262,
+						285
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1362801115599761400,
+		"in_reply_to_status_id_str": "1362801115599761408",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 247,
+			"friends_count": 27,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 70,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 541,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@NASPAtweets @williamandmary @UDelaware Hocutt's service to @NASPAtweets was recognized in 1972 when he was one of the inaugural recipients of the Outstanding Service to NASPA Award which was later renamed the Fred Turner Award for Outstanding Service to NASPA. https://t.co/7ff6dZCKxl"
+	},
+	{
 		"created_at": "Tue Feb 16 18:17:27 +0000 2021",
 		"id": 1361741495838134300,
 		"id_str": "1361741495838134272",
