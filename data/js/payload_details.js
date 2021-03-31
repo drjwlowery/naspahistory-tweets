8422764ef4ab6d657bc94a4763d0d5d5b6583373
drjwlowery <jwlowery@gmail.com>
2021-03-30 21:17:50 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 572,
-	"created_at": "Tue Mar 30 01:17:49 +0000 2021",
+	"tweets": 573,
+	"created_at": "Wed Mar 31 01:17:50 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Tue Mar 30 18:32:00 +0000 2021",
+		"id": 1376965448110071800,
+		"id_str": "1376965448110071813",
+		"full_text": "It's even more striking when one considers that votes to change the name failed in 1948 &amp; 1949. \n\n@NASPAtweets President Lloyd made the case for changing the name arguing that keeping the NADAM name would be to “decline the… responsibilities of national leadership in the field.” https://t.co/EfGozJuh5e",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						102,
+						114
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1376945179744161800,
+					"id_str": "1376945179744161793",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/ExvkWHnWUAEQs9k.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ExvkWHnWUAEQs9k.jpg",
+					"url": "https://t.co/EfGozJuh5e",
+					"display_url": "pic.twitter.com/EfGozJuh5e",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1376965448110071813/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1124,
+							"h": 770,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1124,
+							"h": 770,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 466,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1376945179744161800,
+					"id_str": "1376945179744161793",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/ExvkWHnWUAEQs9k.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ExvkWHnWUAEQs9k.jpg",
+					"url": "https://t.co/EfGozJuh5e",
+					"display_url": "pic.twitter.com/EfGozJuh5e",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1376965448110071813/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1124,
+							"h": 770,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1124,
+							"h": 770,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 466,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1376943775206015000,
+		"in_reply_to_status_id_str": "1376943775206014980",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 253,
+			"friends_count": 31,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 72,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 606,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It's even more striking when one considers that votes to change the name failed in 1948 &amp; 1949. \n\n@NASPAtweets President Lloyd made the case for changing the name arguing that keeping the NADAM name would be to “decline the… responsibilities of national leadership in the field.” https://t.co/EfGozJuh5e"
+	},
+	{
 		"created_at": "Mon Mar 29 15:52:00 +0000 2021",
 		"id": 1376562794800242700,
 		"id_str": "1376562794800242691",
