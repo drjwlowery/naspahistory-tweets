6fec21edbda73f6da80fbe9e5e62113ba61d7919
drjwlowery <jwlowery@gmail.com>
2021-04-19 21:17:51 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 599,
-	"created_at": "Sat Apr 17 01:17:49 +0000 2021",
+	"tweets": 600,
+	"created_at": "Tue Apr 20 01:17:50 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Mon Apr 19 21:48:09 +0000 2021",
+		"id": 1384262570819817500,
+		"id_str": "1384262570819817475",
+		"full_text": "Dr. Joseph A. Park passed away on April 19, 1952 at the age of 58. \n\"Joe\" Park spent his entire career at  @OhioState (1927-1952) where he was the first dean of men &amp; served as @NASPAtweets president  in 1942-1943. \n\nRead about Park's career here: https://t.co/em8VsB334I https://t.co/026ovbdvGM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OhioState",
+					"name": "Ohio State",
+					"id": 18846918,
+					"id_str": "18846918",
+					"indices": [
+						107,
+						117
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						181,
+						193
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/em8VsB334I",
+					"expanded_url": "http://ow.ly/TORV30jloiM",
+					"display_url": "ow.ly/TORV30jloiM",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1384262167419973600,
+					"id_str": "1384262167419973637",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EzXjG20UcAUPQLN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EzXjG20UcAUPQLN.jpg",
+					"url": "https://t.co/026ovbdvGM",
+					"display_url": "pic.twitter.com/026ovbdvGM",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1384262570819817475/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 475,
+							"h": 655,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 475,
+							"h": 655,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 475,
+							"h": 655,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1384262167419973600,
+					"id_str": "1384262167419973637",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EzXjG20UcAUPQLN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EzXjG20UcAUPQLN.jpg",
+					"url": "https://t.co/026ovbdvGM",
+					"display_url": "pic.twitter.com/026ovbdvGM",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1384262570819817475/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 475,
+							"h": 655,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 475,
+							"h": 655,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 475,
+							"h": 655,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 254,
+			"friends_count": 32,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 73,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 640,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dr. Joseph A. Park passed away on April 19, 1952 at the age of 58. \n\"Joe\" Park spent his entire career at  @OhioState (1927-1952) where he was the first dean of men &amp; served as @NASPAtweets president  in 1942-1943. \n\nRead about Park's career here: https://t.co/em8VsB334I https://t.co/026ovbdvGM"
+	},
+	{
 		"created_at": "Fri Apr 16 22:00:36 +0000 2021",
 		"id": 1383178540468871200,
 		"id_str": "1383178540468871171",
