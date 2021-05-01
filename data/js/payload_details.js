e7705bb87866abfa5f276e7b72919c99adbcfaa4
drjwlowery <jwlowery@gmail.com>
2021-04-30 21:17:50 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 609,
-	"created_at": "Tue Apr 27 01:17:49 +0000 2021",
+	"tweets": 610,
+	"created_at": "Sat May 01 01:17:49 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Fri Apr 30 16:48:00 +0000 2021",
+		"id": 1388173299662819300,
+		"id_str": "1388173299662819328",
+		"full_text": "Frank C. (\"Ted\") Baldwin passed away April 30, 1979. He was the 1956-57 @NASPAtweets president.\n\nTed served as the first Dean of Men at Cornell University from 1948 until 1960 when he became Secretary of the University. He retired in 1966. https://t.co/9gfnD26vg0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			239
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						72,
+						84
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1384982429149106200,
+					"id_str": "1384982429149106178",
+					"indices": [
+						240,
+						263
+					],
+					"media_url": "http://pbs.twimg.com/media/EzhyLm2XEAIMrX4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EzhyLm2XEAIMrX4.jpg",
+					"url": "https://t.co/9gfnD26vg0",
+					"display_url": "pic.twitter.com/9gfnD26vg0",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1388173299662819328/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 392,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1043,
+							"h": 602,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1043,
+							"h": 602,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1384982429149106200,
+					"id_str": "1384982429149106178",
+					"indices": [
+						240,
+						263
+					],
+					"media_url": "http://pbs.twimg.com/media/EzhyLm2XEAIMrX4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EzhyLm2XEAIMrX4.jpg",
+					"url": "https://t.co/9gfnD26vg0",
+					"display_url": "pic.twitter.com/9gfnD26vg0",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1388173299662819328/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 392,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1043,
+							"h": 602,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1043,
+							"h": 602,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 254,
+			"friends_count": 32,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 73,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 654,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Frank C. (\"Ted\") Baldwin passed away April 30, 1979. He was the 1956-57 @NASPAtweets president.\n\nTed served as the first Dean of Men at Cornell University from 1948 until 1960 when he became Secretary of the University. He retired in 1966. https://t.co/9gfnD26vg0"
+	},
+	{
 		"created_at": "Mon Apr 26 19:38:47 +0000 2021",
 		"id": 1386766728655429600,
 		"id_str": "1386766728655429640",
