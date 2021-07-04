9bc85845c2a2a2ace844f75b27a6a3e52be0b50b
drjwlowery <jwlowery@gmail.com>
2021-07-03 21:17:50 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 637,
-	"created_at": "Thu Jul 01 01:17:48 +0000 2021",
+	"tweets": 638,
+	"created_at": "Sun Jul 04 01:17:49 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_07.js
@@ -0,0 +1,209 @@
+Grailbird.data.tweets_2021_07 = 
+[
+	{
+		"created_at": "Sat Jul 03 19:43:00 +0000 2021",
+		"id": 1411410162934370300,
+		"id_str": "1411410162934370305",
+		"full_text": "John L. Blackburn passed away on \nJuly 3, 2009 at the age of 84.\n\nHe was the 1973-74 @NASPAtweets president. He spent the bulk of his career at  @UofAlabama. He also worked at Florida State and12 the University of Denver.\nhttps://t.co/qRWrTEKEQJ https://t.co/daO6Gifbdw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			245
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						85,
+						97
+					]
+				},
+				{
+					"screen_name": "UofAlabama",
+					"name": "The Univ. of Alabama",
+					"id": 18685948,
+					"id_str": "18685948",
+					"indices": [
+						145,
+						156
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qRWrTEKEQJ",
+					"expanded_url": "https://blackburninstitute.sa.ua.edu/about/dr-john-l-blackburn-2/",
+					"display_url": "blackburninstitute.sa.ua.edu/about/dr-john-…",
+					"indices": [
+						222,
+						245
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1409593165024219100,
+					"id_str": "1409593165024219142",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/media/E4_hg-cWUAYAH9o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4_hg-cWUAYAH9o.jpg",
+					"url": "https://t.co/daO6Gifbdw",
+					"display_url": "pic.twitter.com/daO6Gifbdw",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1411410162934370305/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 680,
+							"h": 356,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 680,
+							"h": 356,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 356,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1409593165024219100,
+					"id_str": "1409593165024219142",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/media/E4_hg-cWUAYAH9o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4_hg-cWUAYAH9o.jpg",
+					"url": "https://t.co/daO6Gifbdw",
+					"display_url": "pic.twitter.com/daO6Gifbdw",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1411410162934370305/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 680,
+							"h": 356,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 680,
+							"h": 356,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 356,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 257,
+			"friends_count": 32,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 74,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 688,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "John L. Blackburn passed away on \nJuly 3, 2009 at the age of 84.\n\nHe was the 1973-74 @NASPAtweets president. He spent the bulk of his career at  @UofAlabama. He also worked at Florida State and12 the University of Denver.\nhttps://t.co/qRWrTEKEQJ https://t.co/daO6Gifbdw"
+	}
+]
