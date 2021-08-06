e15b3d3c7f08ce2c39acfb75556a76f87abf0f71
drjwlowery <jwlowery@gmail.com>
2021-08-05 21:17:49 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 647,
-	"created_at": "Sat Jul 31 01:17:49 +0000 2021",
+	"tweets": 648,
+	"created_at": "Fri Aug 06 01:17:49 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_08.js
@@ -0,0 +1,211 @@
+Grailbird.data.tweets_2021_08 = 
+[
+	{
+		"created_at": "Thu Aug 05 17:19:52 +0000 2021",
+		"id": 1423332944186417200,
+		"id_str": "1423332944186417153",
+		"full_text": "Dr. Donald Winbigler passed away on August 5, 2020 at the age of 91. He served @NASPAtweets as the 1959-1960 president.\n\nHe spent his career at @Stanford. He joined the faculty in 1940, but spent the majority of his time there as dean of students. https://t.co/7lMDQddaq3 https://t.co/HvZQWnQRpD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						79,
+						91
+					]
+				},
+				{
+					"screen_name": "Stanford",
+					"name": "Stanford University",
+					"id": 18036441,
+					"id_str": "18036441",
+					"indices": [
+						144,
+						153
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/7lMDQddaq3",
+					"expanded_url": "https://news.stanford.edu/news/2004/february11/winbiglerr-211.html",
+					"display_url": "news.stanford.edu/news/2004/febr…",
+					"indices": [
+						248,
+						271
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1423332872828686300,
+					"id_str": "1423332872828686337",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/E8CxtOoXEAEydwL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8CxtOoXEAEydwL.jpg",
+					"url": "https://t.co/HvZQWnQRpD",
+					"display_url": "pic.twitter.com/HvZQWnQRpD",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1423332944186417153/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 1137,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1137,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 478,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1423332872828686300,
+					"id_str": "1423332872828686337",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/E8CxtOoXEAEydwL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8CxtOoXEAEydwL.jpg",
+					"url": "https://t.co/HvZQWnQRpD",
+					"display_url": "pic.twitter.com/HvZQWnQRpD",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1423332944186417153/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 1137,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1137,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 478,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259,
+			"friends_count": 33,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 75,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 702,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dr. Donald Winbigler passed away on August 5, 2020 at the age of 91. He served @NASPAtweets as the 1959-1960 president.\n\nHe spent his career at @Stanford. He joined the faculty in 1940, but spent the majority of his time there as dean of students. https://t.co/7lMDQddaq3 https://t.co/HvZQWnQRpD"
+	}
+]
