03c9602979752f972c4d7abe00af72ac25fa18cd
drjwlowery <jwlowery@gmail.com>
2021-08-10 21:17:49 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 649,
-	"created_at": "Sat Aug 07 01:17:49 +0000 2021",
+	"tweets": 650,
+	"created_at": "Wed Aug 11 01:17:49 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Tue Aug 10 17:40:00 +0000 2021",
+		"id": 1425149948232978400,
+		"id_str": "1425149948232978434",
+		"full_text": "On August 10, 1972, Dr. Scott Goodnight passed away at the age of 97. He served as @NASPAtweets first president (1919-20) and again in 1927-1928. \n\nHe spent his career working at @UWMadison. He joined the faculty in 1901 and became Dean of Men in 1916. He retired in 1945. https://t.co/pZO12plfJB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						83,
+						95
+					]
+				},
+				{
+					"screen_name": "UWMadison",
+					"name": "UW–Madison",
+					"id": 15763818,
+					"id_str": "15763818",
+					"indices": [
+						179,
+						189
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1423378321363116000,
+					"id_str": "1423378321363116035",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/E8DbCrnXIAMXTlu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8DbCrnXIAMXTlu.jpg",
+					"url": "https://t.co/pZO12plfJB",
+					"display_url": "pic.twitter.com/pZO12plfJB",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1425149948232978434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 905,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 905,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 513,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1423378321363116000,
+					"id_str": "1423378321363116035",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/E8DbCrnXIAMXTlu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8DbCrnXIAMXTlu.jpg",
+					"url": "https://t.co/pZO12plfJB",
+					"display_url": "pic.twitter.com/pZO12plfJB",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1425149948232978434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 905,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 905,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 513,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259,
+			"friends_count": 33,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 75,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 705,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On August 10, 1972, Dr. Scott Goodnight passed away at the age of 97. He served as @NASPAtweets first president (1919-20) and again in 1927-1928. \n\nHe spent his career working at @UWMadison. He joined the faculty in 1901 and became Dean of Men in 1916. He retired in 1945. https://t.co/pZO12plfJB"
+	},
+	{
 		"created_at": "Fri Aug 06 17:52:15 +0000 2021",
 		"id": 1423703479139377200,
 		"id_str": "1423703479139377162",
