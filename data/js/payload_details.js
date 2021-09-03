fd9cd26f01ab3bb41f504f5cd176a843cf00a259
drjwlowery <jwlowery@gmail.com>
2021-09-02 21:17:49 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 650,
-	"created_at": "Wed Aug 11 01:17:49 +0000 2021",
+	"tweets": 651,
+	"created_at": "Fri Sep 03 01:17:49 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_09.js
@@ -0,0 +1,201 @@
+Grailbird.data.tweets_2021_09 = 
+[
+	{
+		"created_at": "Thu Sep 02 16:50:39 +0000 2021",
+		"id": 1433472449669181400,
+		"id_str": "1433472449669181444",
+		"full_text": "J. Jörgen Thompson died on Sept. 2, 1963 at the age of 82. He served @NASPAtweets as the 1940-41 president.\n\nThompson spent his career at @StOlaf, joining the faculty in 1909 &amp; retiring in 1956. He was a professor, dean of men, assistant to the president, &amp; acting president. https://t.co/ohaJ8vBZU5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						69,
+						81
+					]
+				},
+				{
+					"screen_name": "StOlaf",
+					"name": "St. Olaf College",
+					"id": 18817325,
+					"id_str": "18817325",
+					"indices": [
+						138,
+						145
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1433472371608928300,
+					"id_str": "1433472371608928261",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/E-S3hvEWQAUhULT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-S3hvEWQAUhULT.jpg",
+					"url": "https://t.co/ohaJ8vBZU5",
+					"display_url": "pic.twitter.com/ohaJ8vBZU5",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1433472449669181444/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 666,
+							"h": 843,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 537,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 666,
+							"h": 843,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1433472371608928300,
+					"id_str": "1433472371608928261",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/E-S3hvEWQAUhULT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-S3hvEWQAUhULT.jpg",
+					"url": "https://t.co/ohaJ8vBZU5",
+					"display_url": "pic.twitter.com/ohaJ8vBZU5",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1433472449669181444/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 666,
+							"h": 843,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 537,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 666,
+							"h": 843,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259,
+			"friends_count": 33,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 75,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 706,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "J. Jörgen Thompson died on Sept. 2, 1963 at the age of 82. He served @NASPAtweets as the 1940-41 president.\n\nThompson spent his career at @StOlaf, joining the faculty in 1909 &amp; retiring in 1956. He was a professor, dean of men, assistant to the president, &amp; acting president. https://t.co/ohaJ8vBZU5"
+	}
+]
