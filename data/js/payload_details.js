18903e41d4a3b21449c65e65c0906f3c40b2d896
drjwlowery <jwlowery@gmail.com>
2021-09-04 21:17:49 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 651,
-	"created_at": "Fri Sep 03 01:17:49 +0000 2021",
+	"tweets": 652,
+	"created_at": "Sun Sep 05 01:17:48 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sat Sep 04 17:19:00 +0000 2021",
+		"id": 1434204359781400600,
+		"id_str": "1434204359781400582",
+		"full_text": "Joseph A. Bursley passed away on Sept. 4, 1950 at the age of 73. He served @NASPAtweets as the 1923-24 president.\n\nBursley spent his career at @UMich. He began teaching Mechanical Engineering in 1904 and was appointed the 1st Dean of Students in 1921. He retired in 1947. https://t.co/Uu7VSIryAo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						75,
+						87
+					]
+				},
+				{
+					"screen_name": "UMich",
+					"name": "University of Michigan",
+					"id": 88836132,
+					"id_str": "88836132",
+					"indices": [
+						143,
+						149
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1433474722319278000,
+					"id_str": "1433474722319278080",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/E-S5qkJXoAAncD4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-S5qkJXoAAncD4.jpg",
+					"url": "https://t.co/Uu7VSIryAo",
+					"display_url": "pic.twitter.com/Uu7VSIryAo",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1434204359781400582/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 247,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 247,
+							"h": 300,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 247,
+							"h": 300,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1433474722319278000,
+					"id_str": "1433474722319278080",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/E-S5qkJXoAAncD4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-S5qkJXoAAncD4.jpg",
+					"url": "https://t.co/Uu7VSIryAo",
+					"display_url": "pic.twitter.com/Uu7VSIryAo",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1434204359781400582/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 247,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 247,
+							"h": 300,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 247,
+							"h": 300,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259,
+			"friends_count": 33,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 75,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 707,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Joseph A. Bursley passed away on Sept. 4, 1950 at the age of 73. He served @NASPAtweets as the 1923-24 president.\n\nBursley spent his career at @UMich. He began teaching Mechanical Engineering in 1904 and was appointed the 1st Dean of Students in 1921. He retired in 1947. https://t.co/Uu7VSIryAo"
+	},
+	{
 		"created_at": "Thu Sep 02 16:50:39 +0000 2021",
 		"id": 1433472449669181400,
 		"id_str": "1433472449669181444",
