5da465ad2ea4a596024617ddad368c6295e5c746
drjwlowery <jwlowery@gmail.com>
2021-11-09 20:17:50 -0500

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 656,
-	"created_at": "Sat Oct 23 01:17:49 +0000 2021",
+	"tweets": 658,
+	"created_at": "Wed Nov 10 01:17:49 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_11.js
@@ -0,0 +1,289 @@
+Grailbird.data.tweets_2021_11 = 
+[
+	{
+		"created_at": "Tue Nov 09 17:56:27 +0000 2021",
+		"id": 1458131384833355800,
+		"id_str": "1458131384833355784",
+		"full_text": "John L. Blackburn spent much of his career at Univ. of Alabama as Dean of Men/Students (1956-69). He served as Vice Chancellor for Student Affairs at the Univ. of Denver from 1969 until 1978 when he returned to Univ. of Alabama. He was the 1973-1974 @NASPAtweets President. https://t.co/BhDem0a1Sl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						250,
+						262
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1458131297809862700,
+					"id_str": "1458131297809862658",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FDxSsJnWEAIWMoM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDxSsJnWEAIWMoM.jpg",
+					"url": "https://t.co/BhDem0a1Sl",
+					"display_url": "pic.twitter.com/BhDem0a1Sl",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1458131384833355784/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 193,
+							"h": 271,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 193,
+							"h": 271,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 193,
+							"h": 271,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1458131297809862700,
+					"id_str": "1458131297809862658",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FDxSsJnWEAIWMoM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDxSsJnWEAIWMoM.jpg",
+					"url": "https://t.co/BhDem0a1Sl",
+					"display_url": "pic.twitter.com/BhDem0a1Sl",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1458131384833355784/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 193,
+							"h": 271,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 193,
+							"h": 271,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 193,
+							"h": 271,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1458130547776053200,
+		"in_reply_to_status_id_str": "1458130547776053253",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 257,
+			"friends_count": 33,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 75,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 715,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "John L. Blackburn spent much of his career at Univ. of Alabama as Dean of Men/Students (1956-69). He served as Vice Chancellor for Student Affairs at the Univ. of Denver from 1969 until 1978 when he returned to Univ. of Alabama. He was the 1973-1974 @NASPAtweets President. https://t.co/BhDem0a1Sl"
+	},
+	{
+		"created_at": "Tue Nov 09 17:53:08 +0000 2021",
+		"id": 1458130547776053200,
+		"id_str": "1458130547776053253",
+		"full_text": "The John L. Blackburn Distinguished Pillars of the Profession recognize previous Pillars of the Profession who continue to grow and strengthen the student affairs. The Distinguished Pillars program was established in 2012.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			222
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1458121290372554800,
+		"in_reply_to_status_id_str": "1458121290372554762",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 257,
+			"friends_count": 33,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 75,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 715,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "The John L. Blackburn Distinguished Pillars of the Profession recognize previous Pillars of the Profession who continue to grow and strengthen the student affairs. The Distinguished Pillars program was established in 2012."
+	}
+]
