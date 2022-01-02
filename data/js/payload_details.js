be163c8143611c56e674bae752c4e7c91037fadc
drjwlowery <jwlowery@gmail.com>
2022-01-01 20:17:49 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 663,
-	"created_at": "Sat Jan 01 01:17:49 +0000 2022",
+	"tweets": 664,
+	"created_at": "Sun Jan 02 01:17:49 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_01.js
@@ -0,0 +1,199 @@
+Grailbird.data.tweets_2022_01 = 
+[
+	{
+		"created_at": "Sat Jan 01 16:40:00 +0000 2022",
+		"id": 1477318701086957600,
+		"id_str": "1477318701086957570",
+		"full_text": "Harold \"Lobby\" Lobdell passed away on January 1, 1963. He served as the 1933-34 @NASPAtweets president.\n\nDean Lobdell worked at @MIT for 42 years. He was known to generations of M.I.T. students, regarded for his salty frankness and loyalty to their interests. https://t.co/E4XALTNwxB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						80,
+						92
+					]
+				},
+				{
+					"screen_name": "MIT",
+					"name": "Massachusetts Institute of Technology (MIT)",
+					"id": 15460048,
+					"id_str": "15460048",
+					"indices": [
+						128,
+						132
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1474052258031673300,
+					"id_str": "1474052258031673355",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/FHTiuARXIAsuyM4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHTiuARXIAsuyM4.jpg",
+					"url": "https://t.co/E4XALTNwxB",
+					"display_url": "pic.twitter.com/E4XALTNwxB",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1477318701086957570/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 224,
+							"h": 328,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 224,
+							"h": 328,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 224,
+							"h": 328,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1474052258031673300,
+					"id_str": "1474052258031673355",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/FHTiuARXIAsuyM4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHTiuARXIAsuyM4.jpg",
+					"url": "https://t.co/E4XALTNwxB",
+					"display_url": "pic.twitter.com/E4XALTNwxB",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1477318701086957570/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 224,
+							"h": 328,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 224,
+							"h": 328,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 224,
+							"h": 328,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 253,
+			"friends_count": 34,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 76,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 723,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Harold \"Lobby\" Lobdell passed away on January 1, 1963. He served as the 1933-34 @NASPAtweets president.\n\nDean Lobdell worked at @MIT for 42 years. He was known to generations of M.I.T. students, regarded for his salty frankness and loyalty to their interests. https://t.co/E4XALTNwxB"
+	}
+]
