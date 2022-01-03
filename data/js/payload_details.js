35e49ce47787fcddf0c9769ade79653685e460c2
drjwlowery <jwlowery@gmail.com>
2022-01-02 20:17:51 -0500

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 664,
-	"created_at": "Sun Jan 02 01:17:49 +0000 2022",
+	"tweets": 666,
+	"created_at": "Mon Jan 03 01:17:50 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,396 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sun Jan 02 21:00:00 +0000 2022",
+		"id": 1477746520631824400,
+		"id_str": "1477746520631824385",
+		"full_text": "On January 2, 2017, Alice R. Manicur passed away. She was @NASPAtweets' first female\nvoting member of the Executive Committee, conference chair (1975), and NASPA President (1976-77).\n\nShe worked at @FrostburgState for 47 years (46 as CSAO). #Trailblazer https://t.co/niLowEuKeG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			253
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Trailblazer",
+					"indices": [
+						241,
+						253
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						58,
+						70
+					]
+				},
+				{
+					"screen_name": "FrostburgState",
+					"name": "Frostburg State University",
+					"id": 64437124,
+					"id_str": "64437124",
+					"indices": [
+						198,
+						213
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1474054339413135400,
+					"id_str": "1474054339413135369",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/FHTknKBX0AkqdRl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHTknKBX0AkqdRl.jpg",
+					"url": "https://t.co/niLowEuKeG",
+					"display_url": "pic.twitter.com/niLowEuKeG",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1477746520631824385/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 302,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1263,
+							"h": 560,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 532,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1474054339413135400,
+					"id_str": "1474054339413135369",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/FHTknKBX0AkqdRl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHTknKBX0AkqdRl.jpg",
+					"url": "https://t.co/niLowEuKeG",
+					"display_url": "pic.twitter.com/niLowEuKeG",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1477746520631824385/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 302,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1263,
+							"h": 560,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 532,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 253,
+			"friends_count": 34,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 76,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 726,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On January 2, 2017, Alice R. Manicur passed away. She was @NASPAtweets' first female\nvoting member of the Executive Committee, conference chair (1975), and NASPA President (1976-77).\n\nShe worked at @FrostburgState for 47 years (46 as CSAO). #Trailblazer https://t.co/niLowEuKeG"
+	},
+	{
+		"created_at": "Sun Jan 02 18:47:00 +0000 2022",
+		"id": 1477713049448423400,
+		"id_str": "1477713049448423424",
+		"full_text": "On January 2, 2002, William S. \"Bill\" Gutherie passed away. He served @NASPAtweets as 1960-61 President.\n\nHe worked at The Ohio State University for 25 years. Then he became board chairman, president and CEO of Buckeye Federal Savings and Loan Association. He retired in 1978. https://t.co/SQOklbYD65",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						70,
+						82
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1474053870619930600,
+					"id_str": "1474053870619930637",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FHTkL3oXMA0lMGu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHTkL3oXMA0lMGu.jpg",
+					"url": "https://t.co/SQOklbYD65",
+					"display_url": "pic.twitter.com/SQOklbYD65",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1477713049448423424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 459,
+							"h": 739,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 459,
+							"h": 739,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 422,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1474053870619930600,
+					"id_str": "1474053870619930637",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FHTkL3oXMA0lMGu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHTkL3oXMA0lMGu.jpg",
+					"url": "https://t.co/SQOklbYD65",
+					"display_url": "pic.twitter.com/SQOklbYD65",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1477713049448423424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 459,
+							"h": 739,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 459,
+							"h": 739,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 422,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 253,
+			"friends_count": 34,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 76,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 726,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On January 2, 2002, William S. \"Bill\" Gutherie passed away. He served @NASPAtweets as 1960-61 President.\n\nHe worked at The Ohio State University for 25 years. Then he became board chairman, president and CEO of Buckeye Federal Savings and Loan Association. He retired in 1978. https://t.co/SQOklbYD65"
+	},
+	{
 		"created_at": "Sat Jan 01 16:40:00 +0000 2022",
 		"id": 1477318701086957600,
 		"id_str": "1477318701086957570",
