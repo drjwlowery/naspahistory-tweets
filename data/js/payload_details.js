303c0a6d72a1a8b9a231e94b06f2a098d92f1c0a
drjwlowery <jwlowery@gmail.com>
2022-02-19 20:17:49 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 689,
-	"created_at": "Fri Feb 18 01:17:49 +0000 2022",
+	"tweets": 690,
+	"created_at": "Sun Feb 20 01:17:49 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sat Feb 19 17:18:00 +0000 2022",
+		"id": 1495085269480972300,
+		"id_str": "1495085269480972293",
+		"full_text": "John E. Hocutt died February 19, 1981 at the age of 66. He was the 1955-56 @NASPAtweets President and has the distinction of serving as the first formally recognized conference chair in 1959. He held administrative positions at @williamandmary and @UDelaware. https://t.co/GmTJ2G239V",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						75,
+						87
+					]
+				},
+				{
+					"screen_name": "williamandmary",
+					"name": "William & Mary",
+					"id": 15768704,
+					"id_str": "15768704",
+					"indices": [
+						228,
+						243
+					]
+				},
+				{
+					"screen_name": "UDelaware",
+					"name": "Univ. of Delaware",
+					"id": 29447487,
+					"id_str": "29447487",
+					"indices": [
+						248,
+						258
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1490429677793595400,
+					"id_str": "1490429677793595400",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/FK8R5TOWUAgbGvn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FK8R5TOWUAgbGvn.jpg",
+					"url": "https://t.co/GmTJ2G239V",
+					"display_url": "pic.twitter.com/GmTJ2G239V",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1495085269480972293/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 680,
+							"h": 363,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 680,
+							"h": 363,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 363,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1490429677793595400,
+					"id_str": "1490429677793595400",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/FK8R5TOWUAgbGvn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FK8R5TOWUAgbGvn.jpg",
+					"url": "https://t.co/GmTJ2G239V",
+					"display_url": "pic.twitter.com/GmTJ2G239V",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1495085269480972293/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 680,
+							"h": 363,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 680,
+							"h": 363,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 363,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 253,
+			"friends_count": 36,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 80,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 757,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "John E. Hocutt died February 19, 1981 at the age of 66. He was the 1955-56 @NASPAtweets President and has the distinction of serving as the first formally recognized conference chair in 1959. He held administrative positions at @williamandmary and @UDelaware. https://t.co/GmTJ2G239V"
+	},
+	{
 		"created_at": "Thu Feb 17 20:08:42 +0000 2022",
 		"id": 1494403452532924400,
 		"id_str": "1494403452532924422",
