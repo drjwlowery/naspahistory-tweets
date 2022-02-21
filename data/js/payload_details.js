5b1b13c8f256204d01bc7716ee536d2529d6752a
drjwlowery <jwlowery@gmail.com>
2022-02-20 20:17:49 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 690,
-	"created_at": "Sun Feb 20 01:17:49 +0000 2022",
+	"tweets": 691,
+	"created_at": "Mon Feb 21 01:17:49 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sun Feb 20 21:49:00 +0000 2022",
+		"id": 1495515856759107600,
+		"id_str": "1495515856759107588",
+		"full_text": "The 1920 @NASPAtweets\n conference began today at \n@Illinois_Alma. The meeting was hosted by Dean Thomas Arkle Clark. The 9 attendees met in Clark's office and discussed a variety of topics including: fraternities, housing, student self-government, &amp; student activities. https://t.co/BZ4361vZR7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						9,
+						21
+					]
+				},
+				{
+					"screen_name": "Illinois_Alma",
+					"name": "Illinois_Alma",
+					"id": 1468225991734710300,
+					"id_str": "1468225991734710273",
+					"indices": [
+						50,
+						64
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1494423789928685600,
+					"id_str": "1494423789928685573",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FL1ChOaXoAUeqT7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FL1ChOaXoAUeqT7.jpg",
+					"url": "https://t.co/BZ4361vZR7",
+					"display_url": "pic.twitter.com/BZ4361vZR7",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1495515856759107588/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1150,
+							"h": 721,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1150,
+							"h": 721,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1494423789928685600,
+					"id_str": "1494423789928685573",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FL1ChOaXoAUeqT7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FL1ChOaXoAUeqT7.jpg",
+					"url": "https://t.co/BZ4361vZR7",
+					"display_url": "pic.twitter.com/BZ4361vZR7",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1495515856759107588/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1150,
+							"h": 721,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1150,
+							"h": 721,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 254,
+			"friends_count": 36,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 80,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 758,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The 1920 @NASPAtweets\n conference began today at \n@Illinois_Alma. The meeting was hosted by Dean Thomas Arkle Clark. The 9 attendees met in Clark's office and discussed a variety of topics including: fraternities, housing, student self-government, &amp; student activities. https://t.co/BZ4361vZR7"
+	},
+	{
 		"created_at": "Sat Feb 19 17:18:00 +0000 2022",
 		"id": 1495085269480972300,
 		"id_str": "1495085269480972293",
