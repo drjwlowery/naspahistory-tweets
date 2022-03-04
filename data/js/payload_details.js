fe3c48f11d38620b9256dd03a16857529b96ce6d
drjwlowery <jwlowery@gmail.com>
2022-03-03 20:17:49 -0500

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 694,
-	"created_at": "Fri Feb 25 01:17:49 +0000 2022",
+	"tweets": 696,
+	"created_at": "Fri Mar 04 01:17:49 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_03.js
@@ -0,0 +1,439 @@
+Grailbird.data.tweets_2022_03 = 
+[
+	{
+		"created_at": "Thu Mar 03 16:59:15 +0000 2022",
+		"id": 1499429206584930300,
+		"id_str": "1499429206584930308",
+		"full_text": "@NASPAtweets @drjwlowery @IUPedu The Outstanding Contribution to Literature and/or Research Award was established in 1968 and was presented for the first time to Dr. E. G. Williamson. In 2011, the award was named in honor of Dr. Kuh, the Chancellor’s Professor Emeritus at @IUBloomington and Director, @NILOA_web. https://t.co/wTLT7TocXc",
+		"truncated": false,
+		"display_text_range": [
+			33,
+			313
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "drjwlowery",
+					"name": "John Wesley Lowery",
+					"id": 120855168,
+					"id_str": "120855168",
+					"indices": [
+						13,
+						24
+					]
+				},
+				{
+					"screen_name": "IUPedu",
+					"name": "Indiana University of Pennsylvania",
+					"id": 16422886,
+					"id_str": "16422886",
+					"indices": [
+						25,
+						32
+					]
+				},
+				{
+					"screen_name": "IUBloomington",
+					"name": "IU Bloomington",
+					"id": 18458348,
+					"id_str": "18458348",
+					"indices": [
+						273,
+						287
+					]
+				},
+				{
+					"screen_name": "NILOA_web",
+					"name": "NILOA",
+					"id": 1444050348,
+					"id_str": "1444050348",
+					"indices": [
+						302,
+						312
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1499429149106225200,
+					"id_str": "1499429149106225153",
+					"indices": [
+						314,
+						337
+					],
+					"media_url": "http://pbs.twimg.com/media/FM8K3eZXwAEOZz6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FM8K3eZXwAEOZz6.jpg",
+					"url": "https://t.co/wTLT7TocXc",
+					"display_url": "pic.twitter.com/wTLT7TocXc",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1499429206584930308/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 149,
+							"h": 208,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 149,
+							"h": 208,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 149,
+							"h": 149,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 149,
+							"h": 208,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1499429149106225200,
+					"id_str": "1499429149106225153",
+					"indices": [
+						314,
+						337
+					],
+					"media_url": "http://pbs.twimg.com/media/FM8K3eZXwAEOZz6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FM8K3eZXwAEOZz6.jpg",
+					"url": "https://t.co/wTLT7TocXc",
+					"display_url": "pic.twitter.com/wTLT7TocXc",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1499429206584930308/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 149,
+							"h": 208,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 149,
+							"h": 208,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 149,
+							"h": 149,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 149,
+							"h": 208,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1499427192404869000,
+		"in_reply_to_status_id_str": "1499427192404869121",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259,
+			"friends_count": 36,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 80,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 766,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@NASPAtweets @drjwlowery @IUPedu The Outstanding Contribution to Literature and/or Research Award was established in 1968 and was presented for the first time to Dr. E. G. Williamson. In 2011, the award was named in honor of Dr. Kuh, the Chancellor’s Professor Emeritus at @IUBloomington and Director, @NILOA_web. https://t.co/wTLT7TocXc"
+	},
+	{
+		"created_at": "Thu Mar 03 16:53:33 +0000 2022",
+		"id": 1499427773957427200,
+		"id_str": "1499427773957427200",
+		"full_text": "@NASPAtweets @drjwlowery @IUPedu George D. Kuh Award recognizes a lifetime commitment to research and scholarship related to higher education by a scholar whose work addresses important and substantial issues, and has had a significant or transformative effect on higher education and student affairs practice. https://t.co/5BQBrssiBv",
+		"truncated": false,
+		"display_text_range": [
+			33,
+			310
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "drjwlowery",
+					"name": "John Wesley Lowery",
+					"id": 120855168,
+					"id_str": "120855168",
+					"indices": [
+						13,
+						24
+					]
+				},
+				{
+					"screen_name": "IUPedu",
+					"name": "Indiana University of Pennsylvania",
+					"id": 16422886,
+					"id_str": "16422886",
+					"indices": [
+						25,
+						32
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1499427736368169000,
+					"id_str": "1499427736368168965",
+					"indices": [
+						311,
+						334
+					],
+					"media_url": "http://pbs.twimg.com/media/FM8JlPiXwAUxzW0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FM8JlPiXwAUxzW0.jpg",
+					"url": "https://t.co/5BQBrssiBv",
+					"display_url": "pic.twitter.com/5BQBrssiBv",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1499427773957427200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 421,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 421,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 421,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1499427736368169000,
+					"id_str": "1499427736368168965",
+					"indices": [
+						311,
+						334
+					],
+					"media_url": "http://pbs.twimg.com/media/FM8JlPiXwAUxzW0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FM8JlPiXwAUxzW0.jpg",
+					"url": "https://t.co/5BQBrssiBv",
+					"display_url": "pic.twitter.com/5BQBrssiBv",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1499427773957427200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 421,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 421,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 421,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1499427192404869000,
+		"in_reply_to_status_id_str": "1499427192404869121",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259,
+			"friends_count": 36,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 80,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 766,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@NASPAtweets @drjwlowery @IUPedu George D. Kuh Award recognizes a lifetime commitment to research and scholarship related to higher education by a scholar whose work addresses important and substantial issues, and has had a significant or transformative effect on higher education and student affairs practice. https://t.co/5BQBrssiBv"
+	}
+]
