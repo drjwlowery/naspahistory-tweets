aa577e0114f87fc8a50b9fdd3bf7c8abe19f33b2
drjwlowery <jwlowery@gmail.com>
2022-04-04 21:17:50 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 715,
-	"created_at": "Thu Mar 24 01:17:49 +0000 2022",
+	"tweets": 716,
+	"created_at": "Tue Apr 05 01:17:49 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_04.js
@@ -0,0 +1,221 @@
+Grailbird.data.tweets_2022_04 = 
+[
+	{
+		"created_at": "Mon Apr 04 20:53:32 +0000 2022",
+		"id": 1511084577510637600,
+		"id_str": "1511084577510637575",
+		"full_text": "@DiverseIssues @NASPAtweets @TempleUniv Dr. Powell was the first African-American woman to serve as the president (now board chair) of @NASPAtweets. https://t.co/USUAE5CzHm",
+		"truncated": false,
+		"display_text_range": [
+			40,
+			148
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DiverseIssues",
+					"name": "Diverse: Issues In Higher Education",
+					"id": 42950839,
+					"id_str": "42950839",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						15,
+						27
+					]
+				},
+				{
+					"screen_name": "TempleUniv",
+					"name": "Temple University",
+					"id": 36445142,
+					"id_str": "36445142",
+					"indices": [
+						28,
+						39
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						135,
+						147
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1511084478009127000,
+					"id_str": "1511084478009126924",
+					"indices": [
+						149,
+						172
+					],
+					"media_url": "http://pbs.twimg.com/media/FPhzU6FXMAwo4WZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPhzU6FXMAwo4WZ.jpg",
+					"url": "https://t.co/USUAE5CzHm",
+					"display_url": "pic.twitter.com/USUAE5CzHm",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1511084577510637575/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 272,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1069,
+							"h": 428,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1069,
+							"h": 428,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1511084478009127000,
+					"id_str": "1511084478009126924",
+					"indices": [
+						149,
+						172
+					],
+					"media_url": "http://pbs.twimg.com/media/FPhzU6FXMAwo4WZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPhzU6FXMAwo4WZ.jpg",
+					"url": "https://t.co/USUAE5CzHm",
+					"display_url": "pic.twitter.com/USUAE5CzHm",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1511084577510637575/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 272,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1069,
+							"h": 428,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1069,
+							"h": 428,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1511060844515410000,
+		"in_reply_to_status_id_str": "1511060844515409922",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 262,
+			"friends_count": 36,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 81,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 794,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			120855168
+		],
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@DiverseIssues @NASPAtweets @TempleUniv Dr. Powell was the first African-American woman to serve as the president (now board chair) of @NASPAtweets. https://t.co/USUAE5CzHm"
+	}
+]
