f716dc46fe4187fc6f6d1b5c51ff499504fa897f
drjwlowery <jwlowery@gmail.com>
2022-04-09 21:17:49 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 718,
-	"created_at": "Sat Apr 09 01:17:49 +0000 2022",
+	"tweets": 719,
+	"created_at": "Sun Apr 10 01:17:48 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Sat Apr 09 18:39:00 +0000 2022",
+		"id": 1512862659376468000,
+		"id_str": "1512862659376467969",
+		"full_text": "Dr. Arno (“Shorty”) Nowotny died on April 9, 1982 at the age of 82. Shorty was active in @NASPAtweets for many years and served as the 1946-47 president.\n\nHe spent his entire career at @UTAustin from 1918 entering as a college freshman until his retirement in 1964. https://t.co/2wZFTD88wq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			265
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						89,
+						101
+					]
+				},
+				{
+					"screen_name": "UTAustin",
+					"name": "UT Austin",
+					"id": 15637375,
+					"id_str": "15637375",
+					"indices": [
+						185,
+						194
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1512132864321667000,
+					"id_str": "1512132864321667078",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/FPws1BcWQAYpPbk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPws1BcWQAYpPbk.jpg",
+					"url": "https://t.co/2wZFTD88wq",
+					"display_url": "pic.twitter.com/2wZFTD88wq",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1512862659376467969/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 437,
+							"h": 543,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 437,
+							"h": 543,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 437,
+							"h": 543,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1512132864321667000,
+					"id_str": "1512132864321667078",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/FPws1BcWQAYpPbk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPws1BcWQAYpPbk.jpg",
+					"url": "https://t.co/2wZFTD88wq",
+					"display_url": "pic.twitter.com/2wZFTD88wq",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1512862659376467969/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 437,
+							"h": 543,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 437,
+							"h": 543,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 437,
+							"h": 543,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 262,
+			"friends_count": 37,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 81,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 797,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dr. Arno (“Shorty”) Nowotny died on April 9, 1982 at the age of 82. Shorty was active in @NASPAtweets for many years and served as the 1946-47 president.\n\nHe spent his entire career at @UTAustin from 1918 entering as a college freshman until his retirement in 1964. https://t.co/2wZFTD88wq"
+	},
+	{
 		"created_at": "Fri Apr 08 18:20:00 +0000 2022",
 		"id": 1512495489735803000,
 		"id_str": "1512495489735802887",
