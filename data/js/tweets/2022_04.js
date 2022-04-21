1ca87b221a183716508660c25a6abdf3922eb9b3
drjwlowery <jwlowery@gmail.com>
2022-04-20 21:17:50 -0400

    Added 1 tweets

--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Wed Apr 20 17:22:00 +0000 2022",
+		"id": 1516829548003930000,
+		"id_str": "1516829548003930112",
+		"full_text": "Dr. Robert M. Strozier passed away on April 20, 1960 at the age of 54 of a heart attack.\n\nStrozier served as Dean of Students at @UChicago from 1946 until 1957, when he became president of @floridastate.\n\nHe also served as the 1953-1954 @NASPAtweets president. https://t.co/po8m7Lekcv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			260
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UChicago",
+					"name": "The University of Chicago",
+					"id": 131144285,
+					"id_str": "131144285",
+					"indices": [
+						129,
+						138
+					]
+				},
+				{
+					"screen_name": "floridastate",
+					"name": "Florida State University",
+					"id": 154533838,
+					"id_str": "154533838",
+					"indices": [
+						189,
+						202
+					]
+				},
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						237,
+						249
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1516492490257404000,
+					"id_str": "1516492490257403906",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/media/FQup4ppXEAI7HYZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQup4ppXEAI7HYZ.jpg",
+					"url": "https://t.co/po8m7Lekcv",
+					"display_url": "pic.twitter.com/po8m7Lekcv",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1516829548003930112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1516492490257404000,
+					"id_str": "1516492490257403906",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/media/FQup4ppXEAI7HYZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQup4ppXEAI7HYZ.jpg",
+					"url": "https://t.co/po8m7Lekcv",
+					"display_url": "pic.twitter.com/po8m7Lekcv",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1516829548003930112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 263,
+			"friends_count": 36,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 81,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 819,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dr. Robert M. Strozier passed away on April 20, 1960 at the age of 54 of a heart attack.\n\nStrozier served as Dean of Students at @UChicago from 1946 until 1957, when he became president of @floridastate.\n\nHe also served as the 1953-1954 @NASPAtweets president. https://t.co/po8m7Lekcv"
+	},
+	{
 		"created_at": "Tue Apr 19 19:01:26 +0000 2022",
 		"id": 1516492182961758200,
 		"id_str": "1516492182961758211",
