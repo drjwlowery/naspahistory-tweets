8d278d4c2debfda4971d80e011e165164f88a0a2
drjwlowery <jwlowery@gmail.com>
2022-05-02 21:17:49 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 749,
-	"created_at": "Sun May 01 01:17:49 +0000 2022",
+	"tweets": 750,
+	"created_at": "Tue May 03 01:17:49 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_05.js
@@ -0,0 +1,189 @@
+Grailbird.data.tweets_2022_05 = 
+[
+	{
+		"created_at": "Mon May 02 19:39:00 +0000 2022",
+		"id": 1521212679435694000,
+		"id_str": "1521212679435694080",
+		"full_text": "@NASPAtweets In his welcome, President Futrall described explaining to a trustee what a dean of men did, \"Engaged extensively in personnel work with the students, &amp; I hoped that the appointment of an officer of that kind in the U. of Arkansas would help us to solve the disciplinary problem.\" https://t.co/qKzL9zrj8h",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			296
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1521207661810827300,
+					"id_str": "1521207661810827266",
+					"indices": [
+						297,
+						320
+					],
+					"media_url": "http://pbs.twimg.com/media/FRxqTwfWQAId6hH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRxqTwfWQAId6hH.jpg",
+					"url": "https://t.co/qKzL9zrj8h",
+					"display_url": "pic.twitter.com/qKzL9zrj8h",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1521212679435694080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 300,
+							"h": 413,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 300,
+							"h": 413,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 300,
+							"h": 413,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1521207661810827300,
+					"id_str": "1521207661810827266",
+					"indices": [
+						297,
+						320
+					],
+					"media_url": "http://pbs.twimg.com/media/FRxqTwfWQAId6hH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRxqTwfWQAId6hH.jpg",
+					"url": "https://t.co/qKzL9zrj8h",
+					"display_url": "pic.twitter.com/qKzL9zrj8h",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1521212679435694080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 300,
+							"h": 413,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 300,
+							"h": 413,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 300,
+							"h": 413,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1521207307794845700,
+		"in_reply_to_status_id_str": "1521207307794845696",
+		"in_reply_to_user_id": 937716186057527300,
+		"in_reply_to_user_id_str": "937716186057527297",
+		"in_reply_to_screen_name": "NASPAhistory",
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 261,
+			"friends_count": 36,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 82,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 833,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@NASPAtweets In his welcome, President Futrall described explaining to a trustee what a dean of men did, \"Engaged extensively in personnel work with the students, &amp; I hoped that the appointment of an officer of that kind in the U. of Arkansas would help us to solve the disciplinary problem.\" https://t.co/qKzL9zrj8h"
+	}
+]
