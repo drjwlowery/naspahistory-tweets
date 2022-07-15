79d7a574d9ea401139335c35eb109b5c010d64d2
drjwlowery <jwlowery@gmail.com>
2022-07-14 21:17:49 -0400

    Added 1 tweets

--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Thu Jul 14 17:34:00 +0000 2022",
+		"id": 1547635536587735000,
+		"id_str": "1547635536587735045",
+		"full_text": "Dean B. A. Tolbert passed away on July 14, 1937 at the age of 55. Tolbert was the 1934-35 @NASPAtweets president.\n\nHe spent his career at @UF. He joined the faculty as assistant professor of education in 1927 and became the dean of students in 1929 when the office was created. https://t.co/f9AXoVk4IQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						90,
+						102
+					]
+				},
+				{
+					"screen_name": "UF",
+					"name": "FLORIDA",
+					"id": 44168607,
+					"id_str": "44168607",
+					"indices": [
+						138,
+						141
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1543584055635202000,
+					"id_str": "1543584055635202050",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FWvphTUX0AIKkCe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWvphTUX0AIKkCe.jpg",
+					"url": "https://t.co/f9AXoVk4IQ",
+					"display_url": "pic.twitter.com/f9AXoVk4IQ",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1547635536587735045/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 560,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 707,
+							"h": 858,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 707,
+							"h": 858,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1543584055635202000,
+					"id_str": "1543584055635202050",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FWvphTUX0AIKkCe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWvphTUX0AIKkCe.jpg",
+					"url": "https://t.co/f9AXoVk4IQ",
+					"display_url": "pic.twitter.com/f9AXoVk4IQ",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1547635536587735045/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 560,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 707,
+							"h": 858,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 707,
+							"h": 858,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 280,
+			"friends_count": 37,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 89,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 876,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dean B. A. Tolbert passed away on July 14, 1937 at the age of 55. Tolbert was the 1934-35 @NASPAtweets president.\n\nHe spent his career at @UF. He joined the faculty as assistant professor of education in 1927 and became the dean of students in 1929 when the office was created. https://t.co/f9AXoVk4IQ"
+	},
+	{
 		"created_at": "Sun Jul 03 17:32:00 +0000 2022",
 		"id": 1543648766573482000,
 		"id_str": "1543648766573481984",
