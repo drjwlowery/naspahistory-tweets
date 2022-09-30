79db6d303135448568113d503c5bfa50b1c49aa9
drjwlowery <jwlowery@gmail.com>
2022-09-29 21:17:49 -0400

    Added 1 tweets

--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Thu Sep 29 17:07:00 +0000 2022",
+		"id": 1575532607437041700,
+		"id_str": "1575532607437041664",
+		"full_text": "Carl W. Knox died on September 29, 1995 at the age of 79. He served @naspatweets as the 1967-68 president.\n\nCarl was the Dean of Students at the Univ. of Illinois. In 1967, he moved to Florida Atlantic Univ. which had opened only 3 years earlier where he served until 1981. https://t.co/v1nt3lGsW9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						68,
+						80
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1575240780603658200,
+					"id_str": "1575240780603658254",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FdxhJNuXEA4D4N2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdxhJNuXEA4D4N2.jpg",
+					"url": "https://t.co/v1nt3lGsW9",
+					"display_url": "pic.twitter.com/v1nt3lGsW9",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1575532607437041664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 604,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1049,
+							"h": 932,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1049,
+							"h": 932,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1575240780603658200,
+					"id_str": "1575240780603658254",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FdxhJNuXEA4D4N2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdxhJNuXEA4D4N2.jpg",
+					"url": "https://t.co/v1nt3lGsW9",
+					"display_url": "pic.twitter.com/v1nt3lGsW9",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1575532607437041664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 604,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1049,
+							"h": 932,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1049,
+							"h": 932,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 283,
+			"friends_count": 38,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 100,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 891,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Carl W. Knox died on September 29, 1995 at the age of 79. He served @naspatweets as the 1967-68 president.\n\nCarl was the Dean of Students at the Univ. of Illinois. In 1967, he moved to Florida Atlantic Univ. which had opened only 3 years earlier where he served until 1981. https://t.co/v1nt3lGsW9"
+	},
+	{
 		"created_at": "Fri Sep 02 16:19:00 +0000 2022",
 		"id": 1565736055495708700,
 		"id_str": "1565736055495708674",
