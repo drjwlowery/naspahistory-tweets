328ec75c8fb91b6d59323e5127ffe838c88f8c5c
drjwlowery <jwlowery@gmail.com>
2022-10-01 21:17:50 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 797,
-	"created_at": "Fri Sep 30 01:17:49 +0000 2022",
+	"tweets": 798,
+	"created_at": "Sun Oct 02 01:17:49 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_10.js
@@ -0,0 +1,199 @@
+Grailbird.data.tweets_2022_10 = 
+[
+	{
+		"created_at": "Sat Oct 01 17:09:00 +0000 2022",
+		"id": 1576257886270853000,
+		"id_str": "1576257886270853128",
+		"full_text": "Chet Peters passed away on October 1, 1995 at the age of 72. He served as @NASPAtweets 1971-72 president.\n\nChet spent his career at @KState. He became the Dean of Students in 1962 and then Vice President for Student Affairs in 1967. He retired in 1985. https://t.co/f8NSe4PdyS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			252
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						74,
+						86
+					]
+				},
+				{
+					"screen_name": "KState",
+					"name": "K-State",
+					"id": 48808600,
+					"id_str": "48808600",
+					"indices": [
+						132,
+						139
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1575241277729345500,
+					"id_str": "1575241277729345546",
+					"indices": [
+						253,
+						276
+					],
+					"media_url": "http://pbs.twimg.com/media/FdxhmJqXEAogyxJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdxhmJqXEAogyxJ.jpg",
+					"url": "https://t.co/f8NSe4PdyS",
+					"display_url": "pic.twitter.com/f8NSe4PdyS",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1576257886270853128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1398,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 464,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 819,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1575241277729345500,
+					"id_str": "1575241277729345546",
+					"indices": [
+						253,
+						276
+					],
+					"media_url": "http://pbs.twimg.com/media/FdxhmJqXEAogyxJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdxhmJqXEAogyxJ.jpg",
+					"url": "https://t.co/f8NSe4PdyS",
+					"display_url": "pic.twitter.com/f8NSe4PdyS",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1576257886270853128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1398,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 464,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 819,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/jDWkdGdBnt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jDWkdGdBnt",
+							"expanded_url": "http://naspa.org",
+							"display_url": "naspa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 283,
+			"friends_count": 38,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 100,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 892,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Chet Peters passed away on October 1, 1995 at the age of 72. He served as @NASPAtweets 1971-72 president.\n\nChet spent his career at @KState. He became the Dean of Students in 1962 and then Vice President for Student Affairs in 1967. He retired in 1985. https://t.co/f8NSe4PdyS"
+	}
+]
