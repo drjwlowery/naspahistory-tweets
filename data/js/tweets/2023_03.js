41b4c51b0b4c984fac8ddbf532ecea9f4bbf5f0f
drjwlowery <jwlowery@gmail.com>
2023-03-07 20:17:50 -0500

    Added 1 tweets

--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Tue Mar 07 19:20:00 +0000 2023",
+		"id": 1633185749192278000,
+		"id_str": "1633185749192278024",
+		"full_text": "Dr. Wesley P. Lloyd passed away on March 7, 1977 at the age of 72. Dr. Lloyd was the 1950-51 @NASPAtweets' president. He spent the bulk of his career at BYU (1937-69) in a variety of roles including Dean of Men, Dean of Students, professor, and Dean of the Graduate School. https://t.co/vV3b5vPLJl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NASPAtweets",
+					"name": "NASPA",
+					"id": 93729105,
+					"id_str": "93729105",
+					"indices": [
+						93,
+						105
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1632818281630544000,
+					"id_str": "1632818281630543872",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/Fqjvk3taMAAj533.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fqjvk3taMAAj533.jpg",
+					"url": "https://t.co/vV3b5vPLJl",
+					"display_url": "pic.twitter.com/vV3b5vPLJl",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1633185749192278024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 614,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1128,
+							"h": 1018,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1128,
+							"h": 1018,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1632818281630544000,
+					"id_str": "1632818281630543872",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/Fqjvk3taMAAj533.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fqjvk3taMAAj533.jpg",
+					"url": "https://t.co/vV3b5vPLJl",
+					"display_url": "pic.twitter.com/vV3b5vPLJl",
+					"expanded_url": "https://twitter.com/NASPAhistory/status/1633185749192278024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 614,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1128,
+							"h": 1018,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1128,
+							"h": 1018,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 937716186057527300,
+			"id_str": "937716186057527297",
+			"name": "NASPA History",
+			"screen_name": "NASPAhistory",
+			"location": "",
+			"description": "Founded in 1919, NASPA is the leading association for the advancement, health, and sustainability of the student affairs profession.",
+			"url": "https://t.co/nt1IV8SxLc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nt1IV8SxLc",
+							"expanded_url": "http://linktr.ee/naspahistory",
+							"display_url": "linktr.ee/naspahistory",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 280,
+			"friends_count": 39,
+			"listed_count": 0,
+			"created_at": "Mon Dec 04 16:12:15 +0000 2017",
+			"favourites_count": 109,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 935,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/937719997421490177/ChhInw3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/937716186057527297/1512404911",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dr. Wesley P. Lloyd passed away on March 7, 1977 at the age of 72. Dr. Lloyd was the 1950-51 @NASPAtweets' president. He spent the bulk of his career at BYU (1937-69) in a variety of roles including Dean of Men, Dean of Students, professor, and Dean of the Graduate School. https://t.co/vV3b5vPLJl"
+	},
+	{
 		"created_at": "Mon Mar 06 18:57:03 +0000 2023",
 		"id": 1632817585665327000,
 		"id_str": "1632817585665327104",
